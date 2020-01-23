a4ad5c987b6df502745833655ab1322f897eb77c
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-01-23 10:47:08 +0200

    Add entity for cloudnet file

---
+++ src/entity/File.ts
@@ -0,0 +1,59 @@
+import {Entity, Column, PrimaryColumn, CreateDateColumn} from "typeorm";
+
+export enum CloudnetFileType {
+    LIDAR = 'lidar',
+    RADAR = 'radar',
+    MODEL = 'model',
+    MWR = 'mwr',
+    CATEGORIZE = 'categorize',
+    LWC = 'lwc',
+    IWC = 'iwc',
+    DRIZZLE = 'drizzle',
+    CLASSIFICATION = 'classification'
+}
+
+export enum FilePublicity {
+    PUBLIC = "public",
+    NO_DL = "nodl",
+    HIDDEN = "hidden"
+}
+
+@Entity()
+export class File {
+
+    @PrimaryColumn("uuid")
+    uuid: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    date: Date;
+
+    @Column()
+    location: string;
+
+    @Column()
+    history: string;
+
+    @Column()
+    filepath: string
+
+    @Column({
+        type: "enum",
+        enum: FilePublicity,
+        default: FilePublicity.PUBLIC
+    })
+    publicity: FilePublicity
+
+    @Column({
+        type: "enum",
+        enum: CloudnetFileType
+    })
+    type: CloudnetFileType
+
+
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
