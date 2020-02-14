c06591bb066010a31d9abdf81614e7feb3ff8355
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-02-14 10:42:27 +0200

    Allow cors on test/dev

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,6 +12,14 @@ async function init() {
 
     const conn = await createConnection(connName)
 
+    if(process.env.NODE_ENV != 'production') {
+        app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        })
+    }
+
     app.get('/file/:uuid', async (req: Request, res: Response) => {
         const repo = conn.getRepository(File)
         repo.findOneOrFail(req.params.uuid)
