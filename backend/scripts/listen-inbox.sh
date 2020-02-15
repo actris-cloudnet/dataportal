e2f94bc598ee01bdc6b59da6336e7f2b747e3d32
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-02-15 07:56:54 +0200

    Use ionotify for inbox listening

--- backend/scripts/listen-inbox.sh
+++ backend/scripts/listen-inbox.sh
@@ -8,4 +8,12 @@ if [ -z $inbox ]; then
 fi
 
 echo "Listening on $inbox"
-fswatch --event Created -e '.*' -i '\.nc$' --print0 $inbox  | xargs -0tn1 scripts/ncdump2node.sh
+
+if [ -x $(which inotifywait) ]; then
+    inotifywait -qme create --format '%w%f' --exclude "[^\.]..$|[^n].$|[^c]$" $inbox | xargs -tn1 -d '\n' scripts/ncdump2node.sh
+elif [ -x $(which fswatch) ]; then
+    fswatch --event Created -e '.*' -i '\.nc$' --print0 $inbox  | xargs -0tn1 scripts/ncdump2node.sh
+else 
+    echo "No suitable watchers found"
+    exit 2
+fi
