9b03d8b771aaeafb94a9e1d7f6b6b380f2cd3ccc
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-02-17 12:51:57 +0200

    Allow selection of input dir via parameter

--- backend/scripts/populate-db.sh
+++ backend/scripts/populate-db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-N_PROCS=${1:-1}
-FILES=$(find data/ -maxdepth 5 -mindepth 5 -name "*.nc")
+INDIR=${1:-"data/"}
+N_PROCS=${2:-1}
+FILES=$(find $INDIR -maxdepth 5 -mindepth 5 -name "*.nc")
 N_FILES=$(printf "%s\n" "${FILES[@]}" | wc -l)
 
 printf "%s\n" "${FILES[@]}" | xargs -I {} -tn1 -P$N_PROCS scripts/ncdump2node.sh {} /dev/null 2>&1 | awk '{printf "%d/%d\r",NR,'$N_FILES'}'
