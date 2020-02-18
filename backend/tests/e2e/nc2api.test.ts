400e2e7c0e82fed7a833a16b96be359fd0c5fda3
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-02-18 18:52:19 +0200

    Use 2-space indent

--- backend/tests/e2e/nc2api.test.ts
+++ backend/tests/e2e/nc2api.test.ts
@@ -1,25 +1,24 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { createConnection } from 'typeorm'
 import * as request from 'request-promise-native'
 import { clearDir, inboxDir, publicDir, clearDb } from '../lib'
 
 beforeAll(async () => {
-    clearDir(inboxDir)
-    clearDir(publicDir)
-    return clearDb()
+  clearDir(inboxDir)
+  clearDir(publicDir)
+  return clearDb()
 })
 
 afterAll(async () => {
 })
 
 describe('after moving a valid NC file to inbox', () => {
-    beforeAll(async () => {
-        fs.copyFileSync('tests/data/20190723_bucharest_classification.nc', path.join(inboxDir, '20190723_bucharest_classification.nc'))
-        return new Promise((resolve, _) => setTimeout(resolve, 3000))
-    })
+  beforeAll(async () => {
+    fs.copyFileSync('tests/data/20190723_bucharest_classification.nc', path.join(inboxDir, '20190723_bucharest_classification.nc'))
+    return new Promise((resolve, _) => setTimeout(resolve, 3000))
+  })
 
-    it('should respond with a corresponding JSON', async () => {
-        return expect(request('http://localhost:3001/file/15506ea8d3574c7baf8c95dfcc34fc7d')).resolves.toBeTruthy()
-    })
+  it('should respond with a corresponding JSON', async () => {
+    return expect(request('http://localhost:3001/file/15506ea8d3574c7baf8c95dfcc34fc7d')).resolves.toBeTruthy()
+  })
 })
--- backend/tests/integration/metadata2db.test.ts
+++ backend/tests/integration/metadata2db.test.ts
@@ -10,59 +10,59 @@ let conn: Connection
 let repo: Repository<File>
 const linkDir = 'tests/data/public'
 beforeAll(async () => {
-    conn = await createConnection('test')
-    await conn.synchronize(true) // Clean db
-    repo = conn.getRepository(File)
+  conn = await createConnection('test')
+  await conn.synchronize(true) // Clean db
+  repo = conn.getRepository(File)
 })
 
 beforeEach(() => {
-    const files = readdirSync(linkDir)
-    for(const file of files) {
-        unlinkSync(join(linkDir, file))
-    }
+  const files = readdirSync(linkDir)
+  for (const file of files) {
+    unlinkSync(join(linkDir, file))
+  }
 })
 
 afterAll(async () => {
-    return conn.close()
+  return conn.close()
 })
 
 function readXmlIn(filename: string, logErrors: boolean): Promise<string> {
-    return new Promise((resolve, _) => {
-        const xml: string = readFileSync(filename, 'utf-8')
-        const proc = spawn('node', ['--no-warnings', 'build/metadata2db.js'])
-        let out: string
-        proc.stderr.on('data', data => {
-            if(logErrors) console.error(data.toString())
-            out += data
-        })
-        proc.on('exit', () => resolve(out))
-        proc.stdin.write(xml)
-        proc.stdin.end()
+  return new Promise((resolve, _) => {
+    const xml: string = readFileSync(filename, 'utf-8')
+    const proc = spawn('node', ['--no-warnings', 'build/metadata2db.js'])
+    let out: string
+    proc.stderr.on('data', data => {
+      if (logErrors) console.error(data.toString())
+      out += data
     })
+    proc.on('exit', () => resolve(out))
+    proc.stdin.write(xml)
+    proc.stdin.end()
+  })
 }
 
-test('errors on missing XML fields', async () =>  {
-    const out = await readXmlIn(bucharestXmlMissing, false)
-    expect(out).toMatch('Failed to import NetCDF XML to DB:')
-    expect(out).toMatch('cloudnet_file_type')
-    return expect(out).toMatch('file_uuid')
+test('errors on missing XML fields', async () => {
+  const out = await readXmlIn(bucharestXmlMissing, false)
+  expect(out).toMatch('Failed to import NetCDF XML to DB:')
+  expect(out).toMatch('cloudnet_file_type')
+  return expect(out).toMatch('file_uuid')
 })
 
 describe('after reading a valid XML', () => {
-    beforeAll(async () => readXmlIn(bucharestXml, true))
+  beforeAll(async () => readXmlIn(bucharestXml, true))
 
 
-    afterAll(async () =>
-        repo.clear()
-    )
+  afterAll(async () =>
+    repo.clear()
+  )
 
-    test('inserts a row to db', async () => {
-        return expect(repo.find()).resolves.toHaveLength(1)
-    })
+  test('inserts a row to db', async () => {
+    return expect(repo.find()).resolves.toHaveLength(1)
+  })
 
-    test('errors when inserting XML with same UUID', async () => {
-        const out = await readXmlIn(bucharestXml, false)
-        expect(out).toMatch('Failed to import NetCDF XML to DB:')
-        return expect(repo.find()).resolves.toHaveLength(1)
-    })
+  test('errors when inserting XML with same UUID', async () => {
+    const out = await readXmlIn(bucharestXml, false)
+    expect(out).toMatch('Failed to import NetCDF XML to DB:')
+    return expect(repo.find()).resolves.toHaveLength(1)
+  })
 })
