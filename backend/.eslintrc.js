d97b3a2c36b956b0676f00e4950e42286663f1a1
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-03-03 11:09:28 +0200

    Apply linting

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,26 +1,27 @@
 const commonRules = require('../.eslintrc-common.js').rules
 
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    rules: {...commonRules}
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'ignorePatterns': ['dist/', 'node_modules/', 'build/', 'public/'],
+  rules: { ...commonRules }
 }
