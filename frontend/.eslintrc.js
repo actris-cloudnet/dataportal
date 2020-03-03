af57c2ce674ea914cf9936e405457d3506513453
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-03-03 11:14:12 +0200

    Fix indentation

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,5 +17,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+  }
 }
