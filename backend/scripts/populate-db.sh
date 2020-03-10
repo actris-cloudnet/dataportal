2b3b49d208560ee528695454df6ac8659de4b9e3
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-03-10 12:16:36 +0200

    Display number of database rows on populate

--- backend/scripts/populate-db.sh
+++ backend/scripts/populate-db.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 INDIR=${1:-"data/"}
 N_PROCS=${2:-1}
-FILES=$(find $INDIR -maxdepth 5 -mindepth 5 -name "*.nc")
+FILES=$(find $INDIR -maxdepth 5 -mindepth 5 -name "*.nc" | sort)
 N_FILES=$(printf "%s\n" "${FILES[@]}" | wc -l)
 
-printf "%s\n" "${FILES[@]}" | xargs -I {} -tn1 -P$N_PROCS scripts/ncdump2node.sh {} /dev/null 2>&1 | awk '{printf "%d/%d\r",NR,'$N_FILES'}'
+printf "%s\n" "${FILES[@]}" \
+    | xargs -I {} -tn1 -P$N_PROCS scripts/ncdump2node.sh {} /dev/null 2>&1 \
+    | xargs -n1 -I% psql dataportal -tAc 'select count(*) from file' \
+    | awk '{printf "%d/%d\t\t%d\r",NR,'$N_FILES',$1}'
 echo
