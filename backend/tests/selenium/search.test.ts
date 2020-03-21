8161159f38c67733db5523cd5c6fc6ba3565c41c
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-03-21 17:02:24 +0200

    Refactor search tests

--- backend/tests/selenium/search.test.ts
+++ backend/tests/selenium/search.test.ts
@@ -7,25 +7,6 @@ let driver: WebDriver
 
 jest.setTimeout(30000)
 
-async function initSearch() {
-  await driver.get('http://localhost:8000/search')
-  const siteSelect = await awaitAndFind(By.className('multiselect'))
-  const dateFrom = await awaitAndFind(By.name('dateFrom'))
-  const dateTo = await awaitAndFind(By.name('dateTo'))
-  await siteSelect.click()
-  await driver.actions().sendKeys(`bucharest${Key.ENTER}`).perform()
-  return {'dateFrom': dateFrom, 'dateTo': dateTo}
-}
-
-async function awaitAndFind(by: By) {
-  await driver.wait(until.elementLocated(by))
-  return driver.findElement(by)
-}
-
-async function getContent() {
-  return await (await awaitAndFind(By.tagName('html'))).getText()
-}
-
 beforeAll(async () => driver = await prepareSelenium())
 
 afterAll(async () => {
@@ -49,9 +30,8 @@ describe('search page', () => {
   })
 
   it('should contain all files with large date span', async () => {
-    const inputs = await initSearch()
-    await inputs['dateFrom'].sendKeys('2010')
-    await driver.actions().sendKeys(Key.TAB).perform()
+    await initSearch()
+    await sendInput('dateFrom', '2010')
     const content = await getContent()
     expect(content).toContain('Found 5 results')
     expect(content).toContain('Classification file from Bucharest')
@@ -61,11 +41,9 @@ describe('search page', () => {
   })
 
   it('should contain correct number of files after setting a date range', async () => {
-    const inputs = await initSearch()
-    await inputs['dateFrom'].sendKeys('2019-07-23')
-    await driver.actions().sendKeys(Key.TAB).perform()
-    await inputs['dateTo'].sendKeys('2019-07-26')
-    await driver.actions().sendKeys(Key.TAB).perform()
+    await initSearch()
+    await sendInput('dateFrom', '2019-07-23')
+    await sendInput('dateTo', '2019-07-26')
     const content = await getContent()
     expect(content).toContain('Found 3 results')
     for (let i = 23; i <= 25; i++) {
@@ -74,52 +52,72 @@ describe('search page', () => {
   })
 
   it('should forward to correct landing page after sorting and clicking certain row', async () => {
-    const inputs = await initSearch()
-    await inputs['dateFrom'].sendKeys('2010')
-    await driver.actions().sendKeys(Key.TAB).perform()
-    await (await awaitAndFind(By.className('b-table-sort-icon-left'))).click()
-    await driver.actions().sendKeys(Key.TAB).perform()
+    await initSearch()
+    await sendInput('dateFrom', '2010')
+    await clickClass('b-table-sort-icon-left')
+    await clickTab()
     driver.actions().click()
-    await (await awaitAndFind(By.xpath('//*[contains(text(), "2019-07-25")]'))).click()
+    await clickXpath('//*[contains(text(), "2019-07-25")]')
     expect(await driver.getCurrentUrl()).toContain('20c03d8f-c9c5-4cb1-8bf8-48d275d621ff')
-    expect(await awaitAndFind(By.id('landing'))).toBeTruthy()
+    expect(await findElement(By.id('landing'))).toBeTruthy()
   })
 
   it('should reset the search after clicking the reset button', async () => {
-    const inputs = await initSearch()
-    await inputs['dateFrom'].sendKeys('2010')
-    await driver.actions().sendKeys(Key.TAB).perform()
-    await (await awaitAndFind(By.id('reset'))).click()
+    await initSearch()
+    await sendInput('dateFrom', '2010')
+    await clickId('reset')
     const content = await getContent()
     expect(content).toContain('No results')
   })
 
   it('should work when clicking the calendar', async () => {
     await initSearch()
-    driver.findElement(By.xpath('//html')).click()
-    await (await awaitAndFind(By.className('calendar'))).click()
-    await (await awaitAndFind(By.className('vc-title'))).click()
-    await (await awaitAndFind(By.className('vc-nav-title'))).click()
-    await (await awaitAndFind(By.className('vc-w-12'))).click()
-    await (await awaitAndFind(By.className('vc-w-12'))).click()
-    await (await awaitAndFind(By.className('day-1'))).click()
+    await setDateFromPast()
     const content = await getContent()
     expect(content).toContain('Found 5 results')
   })
 
   it('correct calendar input should override incorrect keyboard input', async () => {
-    const inputs = await initSearch()
-    await inputs['dateFrom'].sendKeys('2023')
-    await driver.actions().sendKeys(Key.TAB).perform()
-    driver.findElement(By.xpath('//html')).click()
-    await (await awaitAndFind(By.className('calendar'))).click()
-    await (await awaitAndFind(By.className('vc-title'))).click()
-    await (await awaitAndFind(By.className('vc-nav-title'))).click()
-    await (await awaitAndFind(By.className('vc-w-12'))).click()
-    await (await awaitAndFind(By.className('vc-w-12'))).click()
-    await (await awaitAndFind(By.className('day-1'))).click()
+    await initSearch()
+    await sendInput('dateFrom', '2023')
+    await setDateFromPast()
     const content = await getContent()
     expect(content).toContain('Found 5 results')
   })
 
 })
+
+async function initSearch() {
+  await driver.get('http://localhost:8000/search')
+  await clickClass('multiselect')
+  await driver.actions().sendKeys(`bucharest${Key.ENTER}`).perform()
+}
+
+async function setDateFromPast() {
+  await clickXpath('//html')
+  const sequence = ['calendar', 'vc-title', 'vc-nav-title', 'vc-w-12', 'vc-w-12', 'day-1']
+  for (let i=0; i < sequence.length; i++) {
+    await clickClass(sequence[i])
+  }
+}
+
+async function sendInput(key: string, input: string) {
+  const element = await findElement(By.name(key))
+  await element.sendKeys(input)
+  await clickTab()
+}
+
+async function findElement(by: By) {
+  await driver.wait(until.elementLocated(by))
+  return driver.findElement(by)
+}
+
+const getContent = async () => await (await findElement(By.tagName('html'))).getText()
+
+const clickTab = async () => await driver.actions().sendKeys(Key.TAB).perform()
+
+const clickId = async (key: string) => await (await findElement(By.id(key))).click()
+
+const clickClass = async (key: string) => await (await findElement(By.className(key))).click()
+
+const clickXpath = async (key: string) => await (await findElement(By.xpath(key))).click()
