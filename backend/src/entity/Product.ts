d23d3dba60b9905d1a007cc293c9dc4664093c5f
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-04-01 11:41:17 +0300

    Ensure a clean build

---
+++ backend/src/entity/Product.ts
@@ -0,0 +1,18 @@
+import {Entity, Column, PrimaryColumn, OneToMany} from 'typeorm'
+import { File } from './File'
+
+@Entity()
+export class Product {
+
+    @PrimaryColumn()
+    id!: string
+
+    @Column()
+    humanReadableName!: string
+
+    @Column()
+    level!: string
+
+    @OneToMany(_ => File, file => file.site)
+    files!: File[];
+}
