16a046b0675f287433bf6f6944455361128ab068
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-04-06 20:13:37 +0300

    Make dateToUTCString easier to understand

--- backend/src/lib/index.ts
+++ backend/src/lib/index.ts
@@ -1,5 +1,8 @@
 export const stringify = (obj: any): string => JSON.stringify(obj, null, 2)
 
-export const dateToUTCString = (date: string | Date) =>
-  new Date(new Date(date).getTime() - (new Date(date).getTimezoneOffset() * 60000))
+export const dateToUTCString = (date: string | Date) => {
+  const dateDate = new Date(date)
+  const hourInMs = 60 * 1000
+  return new Date(dateDate.getTime() - (dateDate.getTimezoneOffset() * hourInMs))
     .toISOString().replace('T', ' ').replace(/\..*/, '')
+}
