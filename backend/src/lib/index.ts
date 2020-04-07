0105900f4ab4b1c67a56aa9b32887d8fb291675a
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-04-07 08:54:13 +0300

    Rename variable

--- backend/src/lib/index.ts
+++ backend/src/lib/index.ts
@@ -2,7 +2,7 @@ export const stringify = (obj: any): string => JSON.stringify(obj, null, 2)
 
 export const dateToUTCString = (date: string | Date) => {
   const dateDate = new Date(date)
-  const hourInMs = 60 * 1000
-  return new Date(dateDate.getTime() - (dateDate.getTimezoneOffset() * hourInMs))
+  const minuteInMs = 60 * 1000
+  return new Date(dateDate.getTime() - (dateDate.getTimezoneOffset() * minuteInMs))
     .toISOString().replace('T', ' ').replace(/\..*/, '')
 }
