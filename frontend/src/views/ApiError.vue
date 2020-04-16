464459775898c1cc578cec843e3973e2cc9561b0
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-04-16 13:30:24 +0300

    Improve error message

--- frontend/src/views/ApiError.vue
+++ frontend/src/views/ApiError.vue
@@ -18,7 +18,7 @@ section
 <template>
     <main id="error">
         <h1>{{ response.status }}</h1>
-        <h2>{{ response.data }}</h2>
+        <h2>{{ message }}</h2>
         <section v-if="response.status == 404">
         The file you are looking for does not exist.<br>
         If you are looking for the old devcloudnet data portal, click <a href="http://devcloudnet.fmi.fi">here</a>.<br>
@@ -33,5 +33,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class ApiError extends Vue {
   @Prop() response!: Response
+
+  message = typeof this.response.data == 'string' ? this.response.data : this.response.data.errors.join('<br>')
 }
 </script>
