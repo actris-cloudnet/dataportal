4be6961f98d856568f19e56adf2ce0a56ef9ea8d
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-04-17 10:21:24 +0300

    Try inotifywait with close_write

--- backend/scripts/listen-inbox.sh
+++ backend/scripts/listen-inbox.sh
@@ -10,7 +10,7 @@ fi
 echo "Listening on $inbox"
 
 if [ -x $(which inotifywait) ]; then
-    inotifywait -rqme create --format '%w%f' --exclude "[^\.]..$|[^n].$|[^c]$" $inbox | xargs -tn1 -d '\n' scripts/ncdump2node.sh
+    inotifywait -rqme close_write --format '%w%f' --exclude "[^\.]..$|[^n].$|[^c]$" $inbox | xargs -tn1 -d '\n' scripts/ncdump2node.sh
 elif [ -x $(which fswatch) ]; then
     fswatch --event Created -e '.*' -i '\.nc$' --print0 $inbox  | xargs -0tn1 scripts/ncdump2node.sh
 else 
