8b154e9e138fbdb852acd851ea61b98f6ec8a292
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-04-17 12:41:01 +0300

    Fix search keepalive

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -34,7 +34,7 @@ h1, h2, h3, h4, h5, h6
 <template>
   <div id="app">
     <app-header/>
-    <keep-alive include="Search">
+    <keep-alive include="app-search">
       <router-view/>
     </keep-alive>
   </div>
