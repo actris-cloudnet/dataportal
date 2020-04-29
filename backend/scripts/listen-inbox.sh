cc6a2d34ae610437ad05ee341c93136bcbd52da4
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-04-29 11:02:46 +0300

    Listen to all files with fswatch

--- backend/scripts/listen-inbox.sh
+++ backend/scripts/listen-inbox.sh
@@ -12,7 +12,7 @@ echo "Listening on $inbox"
 if [ -x $(which inotifywait) ]; then
     inotifywait -rqme close_write --format '%w%f' $inbox | xargs -tn1 -d '\n' scripts/ncdump2node.sh
 elif [ -x $(which fswatch) ]; then
-    fswatch --event Created -e '.*' -i '\.nc$' --print0 $inbox  | xargs -0tn1 scripts/ncdump2node.sh
+    fswatch --event Created --print0 $inbox  | xargs -0tn1 scripts/ncdump2node.sh
 else 
     echo "No suitable watchers found"
     exit 2
