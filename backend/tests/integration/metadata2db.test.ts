66938c6d5ba06db6c8ede52dd0529f13231f103e
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-05-26 14:07:08 +0300

    Fix integration tests for metadata2db

--- backend/tests/integration/metadata2db.test.ts
+++ backend/tests/integration/metadata2db.test.ts
@@ -3,13 +3,11 @@ import { File } from '../../src/entity/File'
 import { readFileSync } from 'fs'
 import { publicDir, clearDir } from '../lib'
 import { Parser } from 'xml2js'
-import { putRecord } from '../../src/metadata2db'
+import { putRecord, freezeRecord } from '../../src/metadata2db'
 
 const bucharestXml = 'tests/data/20190723_bucharest_classification.xml'
 const bucharestXmlMissing = 'tests/data/20190723_bucharest_classification_missing_fields.xml'
 const bucharestXmlInvalidLocation = 'tests/data/20190723_bucharest_classification_invalid_location.xml'
-const partialLidarXml = 'tests/data/partial_lidar.xml'
-const fullLidarXml = 'tests/data/full_lidar.xml'
 const granadaXml = 'tests/data/20200126_granada_ecmwf.xml'
 const uuid = '15506ea8d3574c7baf8c95dfcc34fc7d'
 const lidarUuid = '04ddd0c2f74e481198465c33b95f06af'
@@ -20,9 +18,10 @@ let repo: Repository<File>
 
 beforeAll(async () => {
   conn = await createConnection('test')
-  //const repo = conn.getRepository('file')
-  //await repo.delete(uuid)
-  //return repo.delete(lidarUuid)
+  repo = conn.getRepository('file')
+  await repo.delete(uuid)
+  await repo.delete(granadaUuid)
+  return repo.delete(lidarUuid)
 })
 
 beforeEach(() => {
@@ -37,81 +36,73 @@ function readJSONIn(filename: string): Promise<string> {
   let parser = new Parser()
   return new Promise((resolve, _) => {
     const xml: string = readFileSync(filename, 'utf-8')
-    let out: string
-    parser.parseString(xml, function (err:any,  result:any) {
+    parser.parseString(xml, function(err:any,  result:any) {
       resolve(result)
-  })
+    })
   })
 }
 
-test('Testing..', async() => {
-  readJSONIn(bucharestXml)
-  .then(result => {
-    return putRecord(conn, result)
-  })
-  .then(result => {
-    expect(result).toMatch('jiihaa')
-  })  
+test('inserting new file', async () => {
+  const input = await readJSONIn(bucharestXml)
+  const out = await putRecord(conn, input)
+  expect(out.status.toString()).toMatch('201')
 })
 
-//test('errors on missing XML fields', async () => {
-//  const out = await readXmlIn(bucharestXmlMissing, false)
-//  expect(out).toMatch('Failed to import NetCDF XML to DB:')
-//  expect(out).toMatch('cloudnet_file_type')
-//  return expect(out).toMatch('file_uuid')
-//})
+test('updating existing file', async () => {
+  const input = await readJSONIn(bucharestXml)
+  const out = await putRecord(conn, input)
+  expect(out.status.toString()).toMatch('200')
+})
 
-/*
+test('freezing existing file', async () => {
+  const freeze = true
+  const pid = 'http://123.123.jeejee'
+  const input = await readJSONIn(bucharestXml)
+  const out_from_put = await putRecord(conn, input)
+  const status = await freezeRecord(out_from_put, conn, pid, freeze)
+  expect(status.toString()).toMatch('200')
+})
+
+test('refuse updating freezed file', async () => {
+  const input = await readJSONIn(bucharestXml)
+  try {
+    await putRecord(conn, input)
+  } catch (e) {
+    expect(e).toMatch('Cannot update a non-volatile file.')
+  }
+})
+
+test('errors on missing header fields', async () => {
+  const input = await readJSONIn(bucharestXmlMissing)
+  try {
+    await putRecord(conn, input)
+  } catch (e) {
+    let msgs = ['Invalid header fields', 'Missing or invalid:', 'cloudnet_file_type', 'file_uuid']
+    msgs.forEach(element => {
+      expect(e).toMatch(element)
+    })
+  }
+})
+
 test('errors on invalid location', async () => {
-  const out = await readXmlIn(bucharestXmlInvalidLocation, false)
-  return expect(out).toMatch('Failed to import NetCDF XML to DB:')
+  await repo.delete(uuid)
+  const input = await readJSONIn(bucharestXmlInvalidLocation)
+  try {
+    await putRecord(conn, input)
+  } catch (e) {
+    expect(e.toString()).toMatch('Could not find any entity of type "Site" matching: "bkrest"')
+  }
 })
 
-test('does not overwrite existing stable files', async () => {
-  await readXmlIn(bucharestXml, true)
-  const now = new Date()
-  await repo.update(uuid, { releasedAt: new Date(new Date(now.setDate(now.getDate() - 2))) })
-  const out = await readXmlIn(bucharestXml, false)
-  expect(out).toMatch('Failed to import NetCDF XML to DB:')
-  return expect(out).toMatch('Cannot update a stable file')
+test('overwrites existing freezed files on test site', async () => {
+  const freeze = true
+  const pid = 'http://123.123.jeejee'
+  const input = await readJSONIn(granadaXml)
+  const out1 = await putRecord(conn, input)
+  const status = await freezeRecord(out1, conn, pid, freeze)
+  expect(status.toString()).toMatch('201')
+  const out2 = await putRecord(conn, input)
+  expect(out2.status.toString()).toMatch('200')
+  expect(out1.body.releasedAt < out2.body.releasedAt)
 })
 
-test('overwrites existing stable files on test site', async () => {
-  await readXmlIn(granadaXml, true)
-  const now = new Date()
-  const oldRelease = (await repo.findOneOrFail(granadaUuid))
-  await repo.update(granadaUuid, { releasedAt: new Date(new Date(now.setDate(now.getDate() - 2))) })
-  await readXmlIn(granadaXml, true)
-  const newRelease = (await repo.findOneOrFail(granadaUuid))
-  return expect(oldRelease.releasedAt.getTime()).toBeLessThan(newRelease.releasedAt.getTime())
-})
-
-describe('after reading a valid XML', () => {
-  let rowN: number
-  beforeAll(async () => {
-    clearDir(publicDir)
-    await repo.delete(uuid)
-    rowN = (await repo.find()).length
-    return readXmlIn(bucharestXml, true)
-  })
-
-  afterAll(async () => {
-    await repo.delete(uuid)
-    return repo.delete(lidarUuid)
-  })
-
-  test('inserts a row to db', async () => {
-    return expect(repo.find()).resolves.toHaveLength(rowN + 1)
-  })
-
-  test('updates row on partial volatile file update', async () => {
-    await readXmlIn(partialLidarXml, true)
-    const oldRelease = (await repo.findOneOrFail(lidarUuid))
-    await readXmlIn(fullLidarXml, true)
-    const newRelease = (await repo.findOneOrFail(lidarUuid))
-    expect(oldRelease.size).toBeLessThan(newRelease.size)
-    expect(oldRelease.checksum).not.toBe(newRelease.checksum)
-    return expect(oldRelease.releasedAt.getTime()).toBeLessThan(newRelease.releasedAt.getTime())
-  })
-})
-*/
