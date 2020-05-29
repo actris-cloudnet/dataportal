47152cdbf86d658a02b32c9fd26955b6c786c00f
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-05-29 11:26:57 +0300

    Make linkFile fail if source file does not exist

--- backend/src/lib/index.ts
+++ backend/src/lib/index.ts
@@ -1,4 +1,6 @@
-import { Connection } from 'typeorm'
+import {Connection} from 'typeorm'
+import {basename, join, resolve as pathResolve} from 'path'
+import {existsSync, promises as fsp, unlinkSync} from 'fs'
 
 export const stringify = (obj: any): string => JSON.stringify(obj, null, 2)
 
@@ -12,4 +14,16 @@ export const dateToUTCString = (date: string | Date) => {
 export const fetchAll = <T>(conn: Connection, schema: Function): Promise<T[]> => {
   const repo = conn.getRepository(schema)
   return repo.find() as Promise<T[]>
+}
+
+const checkFileExists = async (path: string) => fsp.stat(path)
+
+export function linkFile(filename: string, linkPath: string) {
+  const resolvedSource = pathResolve(filename)
+  const fullLink = join(linkPath, basename(filename))
+  return checkFileExists(resolvedSource)
+    .then(() => {
+      if (existsSync(fullLink)) unlinkSync(fullLink)
+      return fsp.symlink(resolvedSource, fullLink)
+    })
 }
