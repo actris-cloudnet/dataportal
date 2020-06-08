2e7b2b0794d7ec8d88032bf3fc93ae2befae34ba
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-06-08 11:34:03 +0300

    Fix linter nag

--- frontend/src/views/Frontpage.vue
+++ frontend/src/views/Frontpage.vue
@@ -47,7 +47,9 @@ main#frontpage
     </header>
     <main>
       <h3>Welcome to the ACTRIS Data Centre node for cloud profiling (CLU).</h3>
+      <!-- eslint-disable-next-line -->
 CLU provides vertical profiles of cloud and turbulent properties at high temporal and spatial resolution from a network of ground-based stations using the Cloudnet processing suite. Major objectives also include the development and validation of new synergetic remote sensing algorithms, and the continuous evaluation of the representation of clouds and other atmospheric parameters in climate and weather forecast models.
+      <!-- eslint-disable-next-line -->
 This node is physically hosted by FMI in Helsinki, Finland. You can find our contact information at the bottom right of this page.
       <div class="buttons">
         <router-link to="search/visualizations" class="download">
