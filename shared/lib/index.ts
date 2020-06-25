af39059e787da0aeba4dac2245528c4dde72cdd3
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-06-25 11:46:06 +0300

    Add a shared directory containing expected API responses

---
+++ shared/lib/index.ts
@@ -0,0 +1,13 @@
+import { promises as fsp } from 'fs'
+import { join } from 'path'
+
+export const readResources = async () => {
+  const baseDir = join(__dirname, '../res')
+  const dirList = await fsp.readdir(baseDir)
+  return Promise.all(dirList.map(fname => fsp.readFile(join(baseDir, fname))))
+    .then(fileContents => {
+      let res: { [key: string]: any } = {}
+      dirList.forEach((fname, i) => (res[fname.substring(0, fname.length - 5)] = JSON.parse(fileContents[i].toString())))
+      return res
+    })
+}
