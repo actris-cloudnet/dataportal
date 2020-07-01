f4869b92ce220a0644eccbb01f61691888f72500
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-07-01 09:24:19 +0300

    Lint

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -6,7 +6,7 @@ export const readResources = async () => {
   const dirList = await fsp.readdir(baseDir)
   return Promise.all(dirList.map(fname => fsp.readFile(join(baseDir, fname))))
     .then(fileContents => {
-      let res: { [key: string]: any } = {}
+      const res: { [key: string]: any } = {}
       dirList.forEach((fname, i) => (res[fname.substring(0, fname.length - 5)] = JSON.parse(fileContents[i].toString())))
       return res
     })
