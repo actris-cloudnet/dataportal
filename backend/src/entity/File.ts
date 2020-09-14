eb24cdc5915c83c26395498ed5025592ea9236b7
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-09-14 14:54:41 +0300

    Add column for source files

--- backend/src/entity/File.ts
+++ backend/src/entity/File.ts
@@ -64,6 +64,9 @@ export class File {
     @Column()
     format!: string
 
+    @Column('text', {array: true})
+    sourceFileIds!: string[]
+
     @OneToMany(_ => Visualization, viz => viz.sourceFile)
     visualizations!: Visualization[]
 
@@ -85,7 +88,8 @@ export class File {
       format: string,
       site: Site,
       product: Product,
-      volatile = true
+      volatile = true,
+      sourceFileIds: string[] = []
     ) {
       // A typeorm hack, see https://github.com/typeorm/typeorm/issues/3903
       if (typeof obj == 'undefined') return
@@ -107,5 +111,6 @@ export class File {
       this.size = filesize
       this.format = format
       this.volatile = volatile
+      this.sourceFileIds = sourceFileIds
     }
 }
---
+++ backend/src/migration/1600084251764-AddSourceFileIds.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddSourceFileIds1600084251764 implements MigrationInterface {
+    name = 'AddSourceFileIds1600084251764'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "file" ADD "sourceFileIds" text array NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "file" DROP COLUMN "sourceFileIds"`);
+    }
+
+}
