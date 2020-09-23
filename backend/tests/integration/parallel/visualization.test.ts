20e27ba5ed159b6bc1a5322a1cf6084e872fcf43
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-09-23 14:41:45 +0300

    Fixed visualization test response ordering Hopefully once and for all

--- backend/tests/integration/parallel/visualization.test.ts
+++ backend/tests/integration/parallel/visualization.test.ts
@@ -4,6 +4,23 @@ import {backendPublicUrl} from '../../lib'
 const url = `${backendPublicUrl}visualizations/`
 const headers = { 'content-type': 'application/json'}
 
+const expectedResult = {
+  sourceFileId: '38092c00-161d-4ca2-a29d-628cf8e960f6',
+  locationHumanReadable: 'Mace Head',
+  productHumanReadable: 'Radar',
+  visualizations: [
+    {
+      filename: 'test1.png',
+      'productVariable': {'id': 'test2', 'humanReadableName': 'Kaljanhimo', 'order': '0'}
+    },
+    {
+      filename: 'test0.png',
+      'productVariable': {'id': 'test1', 'humanReadableName': 'Auringonpaisteen määrä', 'order': '1'}
+    },
+  ]
+}
+
+
 describe('GET /visualizations', () => {
 
   it('on no results returns empty list and responds with 200', async () => {
@@ -14,21 +31,6 @@ describe('GET /visualizations', () => {
 
   it('on valid search returns correct list of visualizations and responds with 200', async () => {
 
-    const expectedResult = {
-      sourceFileId: '38092c00-161d-4ca2-a29d-628cf8e960f6',
-      locationHumanReadable: 'Mace Head',
-      productHumanReadable: 'Radar',
-      visualizations: [
-        {
-          filename: 'test0.png',
-          'productVariable': {'id': 'test1', 'humanReadableName': 'Auringonpaisteen määrä', 'order': '1'}
-        },
-        {
-          filename: 'test1.png',
-          'productVariable': {'id': 'test2', 'humanReadableName': 'Kaljanhimo', 'order': '0'}
-        },
-      ]
-    }
     const res = await axios.get(url, {headers, params: {product: 'radar'}})
     expect(res.status).toEqual(200)
     return expect(res.data).toMatchObject([expectedResult])
@@ -37,21 +39,6 @@ describe('GET /visualizations', () => {
 
 describe('GET /visualizations/:uuid', () => {
 
-  const expectedResult = {
-    sourceFileId: '38092c00-161d-4ca2-a29d-628cf8e960f6',
-    locationHumanReadable: 'Mace Head',
-    productHumanReadable: 'Radar',
-    visualizations: [
-      {
-        filename: 'test0.png',
-        'productVariable': {'id': 'test1', 'humanReadableName': 'Auringonpaisteen määrä', 'order': '1'}
-      },
-      {
-        filename: 'test1.png',
-        'productVariable': {'id': 'test2', 'humanReadableName': 'Kaljanhimo', 'order': '0'}
-      },
-    ]
-  }
 
   it('returns correct list of visualizations and responds with 200', async () => {
     const res = await axios.get(`${url}${expectedResult.sourceFileId}`)
