5b4c97cc2498f42a7c2d96675336f902a9934c3c
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-10-01 14:10:54 +0300

    Add default to instruments

--- backend/src/migration/1601540932396-AddHumanReadableNameToInstruments.ts
+++ backend/src/migration/1601540932396-AddHumanReadableNameToInstruments.ts
@@ -4,7 +4,7 @@ export class AddHumanReadableNameToInstruments1601540932396 implements Migration
     name = 'AddHumanReadableNameToInstruments1601540932396'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "instrument" ADD "humanReadableName" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "instrument" ADD "humanReadableName" character varying NOT NULL DEFAULT ''`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
