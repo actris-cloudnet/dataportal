6804891a35cb9aff32f66de643108c3072262eae
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-10-14 10:55:48 +0300

    Add tests for GETting collections

---
+++ backend/tests/integration/parallel/collection.test.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import {backendPublicUrl, genResponse} from '../../lib'
+import {readResources} from '../../../../shared/lib'
+
+const url = `${backendPublicUrl}collection/`
+const validCollectionUuid = '48092c00-161d-4ca2-a29d-628cf8e960f6'
+let allCollections: any
+
+describe('GET /api/collection/:uuid', () => {
+  beforeAll(async () => (allCollections = (await readResources())['allcollections']))
+
+  it('returns a valid collection', async () => {
+    const res = await axios.get(`${url}${validCollectionUuid}`)
+    expect(res.data).toMatchObject(allCollections[0])
+  })
+
+  it('responds 404 if collection is not found', async () => {
+    return expect(axios.get(`${url}88092c00-161d-4ca2-a29d-628cf8e960f6`)).rejects
+      .toMatchObject(genResponse(404, {errors: ['Collection not found']}))
+  })
+})
