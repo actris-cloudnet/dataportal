b6e102d63f31d84181b25249af1b1794cbafbe94
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2020-11-18 11:29:38 +0200

    Update tests

--- backend/tests/e2e/nc2api.test.ts
+++ backend/tests/e2e/nc2api.test.ts
@@ -1,16 +1,49 @@
 import * as fs from 'fs'
 import axios from 'axios'
-import { clearDir, publicDir, clearRepo, backendPrivateUrl, backendPublicUrl, fileServerUrl, runNcdump } from '../lib'
+import { clearDir, publicDir, clearRepo, backendPrivateUrl, backendPublicUrl, runNcdump } from '../lib'
 import * as AdmZip from 'adm-zip'
 import { createHash } from 'crypto'
+import * as express from 'express'
+import {Server} from 'http'
+import {Connection, createConnection, Repository} from 'typeorm'
+import {Download} from '../../src/entity/Download'
 
-beforeAll(async () => {
-  clearDir(publicDir)
-  await clearRepo('visualization')
-  return clearRepo('file')
+let conn: Connection
+let repo: Repository<Download>
+
+let server: Server
+let serverMemory: any = {}
+beforeAll(next => {
+  Promise.all([
+    createConnection('test'),
+    clearRepo('download'),
+    clearRepo('visualization'),
+    clearRepo('file')
+  ]).then(([newConn, _1, _2]) => {
+    conn = newConn
+    repo = conn.getRepository('download')
+    const app = express()
+    app.put('/*', (req, res, _next) =>{
+      const path = req.params[0]
+      serverMemory[path] = new Buffer(0)
+      req.on('data', chunk => (serverMemory[path] = Buffer.concat([serverMemory[path], chunk])))
+      req.on('error', console.error)
+      req.on('end', () => res.sendStatus(201))
+    })
+    app.get('/*', (req, res, _next) =>{
+      res.send(serverMemory[req.params[0]])
+    })
+    server = app.listen(5910, next)
+    return
+  })
 })
 
-afterAll(async () => {
+afterAll(next => {
+  clearRepo('download').then(() =>
+  clearRepo('visualization')).then(() =>
+  clearRepo('file')).then(() =>
+  conn.close().then(() =>
+  server.close(() => next())))
 })
 
 const expectedJson = {
@@ -45,8 +78,10 @@ const axiosConfig = { headers: { 'content-type': 'application/xml'}}
 
 describe('after PUTting metadata to API', () => {
   beforeAll(async () => {
-    const xmlOut = await runNcdump('tests/data/20190723_bucharest_classification.nc')
-    return axios.put(`${backendPrivateUrl}files/${expectedJson.uuid}`, xmlOut, axiosConfig)
+    const filepath = 'tests/data/20190723_bucharest_classification.nc'
+    const xmlOut = await runNcdump(filepath)
+    await axios.put(`${backendPrivateUrl}files/${expectedJson.uuid}`, xmlOut, axiosConfig)
+    return axios.put(`http://localhost:5910/cloudnet-product/${expectedJson.uuid}`, fs.createReadStream(filepath))
   })
 
   it('responds with a corresponding metadata JSON', async () => {
@@ -55,12 +90,15 @@ describe('after PUTting metadata to API', () => {
       .then(response => expect(response.data).toMatchObject(expectedJson))
   })
 
-  it('serves the file', async () => {
+  it('serves the file and increases download count', async () => {
     return axios
-      .head(`${fileServerUrl}${expectedJson.filename}`)
+      .get(`${backendPublicUrl}download/product/${expectedJson.uuid}/${expectedJson.filename}`, {responseType: 'arraybuffer'})
       .then(response => {
         expect(response.status).toEqual(200)
-        expect(parseInt(response.headers['content-length'])).toEqual(expectedJson.size)
+        const hash = createHash('sha256')
+        hash.update(response.data)
+        expect(hash.digest('hex')).toEqual(expectedJson.checksum)
+        return expect(repo.findOne({objectUuid: expectedJson.uuid})).resolves.toBeTruthy()
       })
   })
 
@@ -68,8 +106,10 @@ describe('after PUTting metadata to API', () => {
     let collectionUuid = ''
     beforeAll(async () => {
       const uuid = '926ba5cb19e241538d879d97596f5574'
-      const xmlOut = await runNcdump('tests/data/20190724_bucharest_classification.nc')
+      const filepath = 'tests/data/20190724_bucharest_classification.nc'
+      const xmlOut = await runNcdump(filepath)
       await axios.put(`${backendPrivateUrl}files/${uuid}`, xmlOut, axiosConfig)
+      await axios.put(`http://localhost:5910/cloudnet-product/${uuid}`, fs.createReadStream(filepath))
       const res =  await axios.post(`${backendPublicUrl}collection/`, {files: [expectedJson.uuid, uuid]})
       collectionUuid = res.data
     })
@@ -78,7 +118,7 @@ describe('after PUTting metadata to API', () => {
       const tmpZip = 'tests/data/tmp.zip'
       const now = new Date().getTime()
       const shas = await (await axios.get(`${backendPublicUrl}files/`, { params: { location: 'bucharest' } })).data.map((file: any) => file.checksum)
-      const receivedFile = await axios.get(`${backendPublicUrl}download/${collectionUuid}`, { responseType: 'arraybuffer'})
+      const receivedFile = await axios.get(`${backendPublicUrl}download/collection/${collectionUuid}`, { responseType: 'arraybuffer'})
       fs.writeFileSync(tmpZip, receivedFile.data)
       new AdmZip(tmpZip).getEntries().map((entry, i) => {
         const hash = createHash('sha256')
@@ -86,10 +126,7 @@ describe('after PUTting metadata to API', () => {
         expect(hash.digest('hex')).toEqual(shas[shas.length - 1 - i])
       })
       fs.unlinkSync(tmpZip)
-      const {data} = await axios.get(`${backendPublicUrl}collection/${collectionUuid}`)
-      expect(data.downloadCount).toEqual(1)
-      expect(new Date(data.updatedAt).getTime()).toBeGreaterThan(now)
-    }
-    )
+      return expect(repo.findOne({objectUuid: collectionUuid})).resolves.toBeTruthy()
+    })
   })
 })
