4e34ca3e81dec7933ff0316c4f16bcb3177f1b8c
Anniina Korpinen <anniina.korpinen@fmi.fi>
2021-03-02 11:59:21 +0200

    Block never-ending map shifting of right and left

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -142,7 +142,8 @@ export default class Map extends Vue {
   // init
   initMap() {
     this.legend.onAdd = this.generateLegend
-    this.map = L.map(this.$refs['mapElement'] as HTMLElement).setView(this.getMapCenter(), this.zoom)
+    this.map = L.map(this.$refs['mapElement'] as HTMLElement, {
+      maxBounds: this.setMapBounds()}).setView(this.getMapCenter(), this.zoom)
     this.tileLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png')
     this.tileLayer.addTo(this.map)
     if (this.showLegend) this.legend.addTo(this.map)
@@ -195,6 +196,13 @@ export default class Map extends Vue {
     if (this.showLegend)
       center = [34.00, 14.00]
     return center
+  }
+
+  setMapBounds() {
+    var southWest = L.latLng(-90.00, -330.00),
+    northEast = L.latLng(90.00, 330.00),
+    bounds = L.latLngBounds(southWest, northEast)
+    return bounds
   }
 
   generateLegend() {
