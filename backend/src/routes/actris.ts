6a3de9561fe37a3b8249210fbdb3a6da793c838a
Simo Tukiainen <simo.tukiainen@fmi.fi>
2021-06-04 11:17:23 +0300

    Use legacy field

--- backend/src/routes/actris.ts
+++ backend/src/routes/actris.ts
@@ -134,7 +134,7 @@ export class ActrisRoutes {
         'sub_matrix': 'Unknown', // mandatory
         'instrument_type': [(file.product.level == '2') || (file.product.id == 'model') ? 'UNKNOWN' : file.product], // mandatory
         'program_affiliation': ['ACTRIS'], // mandatory, fixed list ['ACTRIS', 'AMAP', 'AMAP_public','EUSAAR','EMEP','ACTRIS_preliminary','GAW-WDCA','GAW-WDCRG','NOAA-ESRL']
-        'legacy_data': false, // mandatory
+        'legacy_data': file.legacy, // mandatory
         'data_level': file.product.level[0],   // mandatory, fixed list [0, 1, 2, 3]
         'data_sublevel': file.product.level[1] || null,  // optional
         'data_product': 'near-realtime-data' // mandatory, need fixed list e.g. ['higher level data','quality assured data', 'near-realtime-data']
