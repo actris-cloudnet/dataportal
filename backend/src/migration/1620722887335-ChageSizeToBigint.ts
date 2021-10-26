e6f55bbddb5c84191fc430db945614ec0fd67e9d
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-10-26 11:13:37 +0300

    Fix bigint migration

--- backend/src/migration/1620722887335-ChageSizeToBigint.ts
+++ backend/src/migration/1620722887335-ChageSizeToBigint.ts
@@ -5,6 +5,8 @@ export class ChageSizeToBigint1620722887335 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "upload" ALTER COLUMN "size" TYPE bigint`);
+        await queryRunner.query(`ALTER TABLE "instrument_upload" ALTER COLUMN "size" TYPE bigint`);
+        await queryRunner.query(`ALTER TABLE "model_upload" ALTER COLUMN "size" TYPE bigint`);
         await queryRunner.query(`ALTER TABLE "file" ALTER COLUMN "size" TYPE bigint`);
         await queryRunner.query(`ALTER TABLE "regular_file" ALTER COLUMN "size" TYPE bigint`);
         await queryRunner.query(`ALTER TABLE "model_file" ALTER COLUMN "size" TYPE bigint`);
