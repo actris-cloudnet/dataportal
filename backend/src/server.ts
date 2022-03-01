ffc210ceec1b6a0741c0999ebf3e156dedaebd1a
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-03-01 11:09:32 +0200

    Add delete route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -179,6 +179,7 @@ import env from './lib/env'
   app.get('/file-dateforsize', fileRoutes.dateforsize)
   app.put('/quality/:uuid', express.json(), qualityRoutes.putQualityReport)
   app.get('/api/download/stats', authMiddleware, dlRoutes.stats)
+  app.delete('/files/:uuid', fileRoutes.deleteFile)
 
   app.use(errorHandler)
 
