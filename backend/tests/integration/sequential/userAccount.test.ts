4f3ad3aff2adddc24ca4a3649db48b274fc585a8
Niko Leskinen <niko.leskinen@fmi.fi>
2022-05-03 14:42:56 +0300

    Prettier+Lint-fix

--- backend/tests/integration/sequential/userAccount.test.ts
+++ backend/tests/integration/sequential/userAccount.test.ts
@@ -1,27 +1,24 @@
 import axios from 'axios'
 import { readFileSync } from 'fs'
 import { backendPrivateUrl } from '../../lib'
-const md5 = require('apache-md5')
-
 
 const USER_ACCOUNTS_URL = `${backendPrivateUrl}user-accounts`
-
 
 beforeAll(async () => {
   let req = await axios.get(USER_ACCOUNTS_URL)
   // Remove users from the db in the beginning
-  for (const user of req.data){
-    await axios.delete(USER_ACCOUNTS_URL.concat('/',user.id))
+  for (const user of req.data) {
+    await axios.delete(USER_ACCOUNTS_URL.concat('/', user.id))
   }
 })
 
-describe ('userAccount tests', () => {
+describe('userAccount tests', () => {
   it('responds with zero user at the beginning', async () => {
     let req = await axios.get(USER_ACCOUNTS_URL)
     expect(req.data).toHaveLength(0)
   })
   it('adds three users succesfully', async () => {
-    const rawData = readFileSync( 'tests/data/userAccountCredentials.json', 'utf8')
+    const rawData = readFileSync('tests/data/userAccountCredentials.json', 'utf8')
     const data = JSON.parse(rawData)
     expect(data).toHaveLength(3)
     let resp = await axios.post(USER_ACCOUNTS_URL, data)
@@ -29,14 +26,14 @@ describe ('userAccount tests', () => {
   })
   it('responds with three users', async () => {
     let req = await axios.get(USER_ACCOUNTS_URL)
-    expect(req.data).toHaveLength(3) 
+    expect(req.data).toHaveLength(3)
   })
   it('deletes all users one at the time', async () => {
     let req = await axios.get(USER_ACCOUNTS_URL)
     let nUsers = req.data.length
     expect(nUsers).toBe(3)
-    for (const user of req.data){
-      await axios.delete(USER_ACCOUNTS_URL.concat('/',user.id))
+    for (const user of req.data) {
+      await axios.delete(USER_ACCOUNTS_URL.concat('/', user.id))
       let reqGet = await axios.get(USER_ACCOUNTS_URL)
       expect(reqGet.data).toHaveLength(--nUsers)
     }
