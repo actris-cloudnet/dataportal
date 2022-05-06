78f74522227c73849261e37b802e17d5282bc5c2
Niko Leskinen <niko.leskinen@fmi.fi>
2022-05-06 11:25:14 +0300

    Add possibility to bypass the authentication

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -6,28 +6,47 @@ const md5 = require('apache-md5')
 const { timingSafeEqual } = require('crypto')
 const { Buffer } = require('buffer')
 
+const BYPASS_AUTHENTICATOR: boolean = true
+const BYPASS_AUTHORIZATOR: boolean = true
+
 export class Authenticator {
   private userAccountRepository: Repository<UserAccount>
+  private logIdentifier: string
+  private bypass: boolean
 
   constructor(conn: Connection) {
     this.userAccountRepository = conn.getRepository<UserAccount>('user_account')
+    this.logIdentifier = 'MessageFromAuthenticator: '
+    this.bypass = BYPASS_AUTHENTICATOR
   }
 
   middleware: RequestHandler = async (req, res, next) => {
     // Check that request has credentials included
     let credentials = auth(req)
     if (!credentials) {
-      res.status(401).send('Unauthorized')
-      return
+      if (this.bypass) {
+        console.log(this.logIdentifier.concat('Unauthorized: no credentials'))
+        next()
+        return
+      } else {
+        res.status(401).send('Unauthorized')
+        return
+      }
     }
     let userAccount: UserAccount | undefined = await this.userAccountRepository
       .createQueryBuilder('user_account')
       .where('user_account.username = :username', { username: credentials.name })
       .getOne()
     // Check that user exists in the database
-    if (userAccount === undefined || credentials.name != userAccount!.username) {
-      res.status(401).send('Unauthorized')
-      return
+    if (userAccount === undefined) {
+      if (this.bypass) {
+        console.log(this.logIdentifier.concat('Unauthorized: username not found'))
+        next()
+        return
+      } else {
+        res.status(401).send('Unauthorized')
+        return
+      }
     }
     // Check that password in the request is correct
     if (
@@ -36,11 +55,23 @@ export class Authenticator {
         Buffer.from(userAccount!.passwordHash)
       )
     ) {
-      next()
-      return
+      if (this.bypass) {
+        console.log(this.logIdentifier.concat('Authentication succesful')) // Remove when ready
+        next()
+        return
+      } else {
+        next()
+        return
+      }
     } else {
-      res.status(401).send('Unauthorized')
-      return
+      if (this.bypass) {
+        console.log(this.logIdentifier.concat('Unauthorized: passwordhashes unequal'))
+        next()
+        return
+      } else {
+        res.status(401).send('Unauthorized')
+        return
+      }
     }
   }
 }
