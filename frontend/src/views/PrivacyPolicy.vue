eb0d8ca360274900979556a9065d24bf3dd04de4
Tuomas Siipola <tuomas.siipola@fmi.fi>
2022-06-30 15:23:37 +0300

    Use p tags in privacy policy

--- frontend/src/views/PrivacyPolicy.vue
+++ frontend/src/views/PrivacyPolicy.vue
@@ -24,27 +24,35 @@ h4
 
 <template>
   <main id="privacypolicy">
-    <!-- eslint-disable max-len -->
     <h2>Cloudnet privacy policy</h2>
     <h3>Scope</h3>
-    This policy describes how we, ACTRIS Cloud remote sensing data centre unit (later Cloudnet) at the Finnish
-    Meteorological Institute (later FMI), use your personal data when you visit the Cloudnet website cloudnet.fmi.fi
-    (later Cloudnet data portal).
+    <p>
+      This policy describes how we, ACTRIS Cloud remote sensing data centre unit (later Cloudnet) at the Finnish
+      Meteorological Institute (later FMI), use your personal data when you visit the Cloudnet website cloudnet.fmi.fi
+      (later Cloudnet data portal).
+    </p>
     <h3>Who is responsible for my data?</h3>
-    The Cloudnet unit at FMI is the data controller. That means we are responsible for collecting your data and looking
-    after it.
+    <p>
+      The Cloudnet unit at FMI is the data controller. That means we are responsible for collecting your data and
+      looking after it.
+    </p>
     <h3>What data is used?</h3>
-    Your IP address is recorded when you are using the Cloudnet data portal. We use IP addresses to extract metrics of
-    site use, such as which country our users connect from, and how many unique users have downloaded data files.
-    Processing is based on our interest in Cloudnet data spread, and in the need to prevent the malicious use of the
-    site (e.g. denial of service attacks).
+    <p>
+      Your IP address is recorded when you are using the Cloudnet data portal. We use IP addresses to extract metrics of
+      site use, such as which country our users connect from, and how many unique users have downloaded data files.
+      Processing is based on our interest in Cloudnet data spread, and in the need to prevent the malicious use of the
+      site (e.g. denial of service attacks).
+    </p>
     <h3>Who gets my data?</h3>
-    Personal data will be used for the purpose it was first collected for. We won’t transfer your personal information
-    outside of Cloudnet.
-
+    <p>
+      Personal data will be used for the purpose it was first collected for. We won’t transfer your personal information
+      outside of Cloudnet.
+    </p>
     <h3>Data security</h3>
-    The Cloudnet data portal uses SSL encryption. The Cloudnet data portal servers are only accessible for admins from
-    the FMI’s internal network. The servers are physically located in Finland.
+    <p>
+      The Cloudnet data portal uses SSL encryption. The Cloudnet data portal servers are only accessible for admins from
+      the FMI’s internal network. The servers are physically located in Finland.
+    </p>
     <h3>What rights do I have?</h3>
     <h4>Unconditional rights by law</h4>
     <ul>
@@ -65,7 +73,7 @@ h4
       </li>
     </ul>
     <h4>Conditional rights</h4>
-    These rights apply if certain conditions are met.
+    <p>These rights apply if certain conditions are met.</p>
     <ul>
       <li>
         Erasure. You might have the right to have your data removed from our storage, for example when you withdraw
@@ -81,19 +89,22 @@ h4
       </li>
     </ul>
     <h3>Exercising your rights</h3>
-    If you have a question regarding our privacy policy, wish to exercise any of your rights, or feel dissatisfaction
-    with our use of your data, please contact the Cloudnet team by sending an email to
-    <a href="mailto:actris-cloudnet-feedback@fmi.fi">actris-cloudnet-feedback@fmi.fi</a>. We will perform our duties
-    without undue delay within one month of your request. That period may be extended by two further months where
-    necessary, considering the complexity and number of the requests. For further information about your rights and
-    exercising them, please see here
-    <a href="https://tietosuoja.fi/en/know-your-rights">https://tietosuoja.fi/en/know-your-rights</a>.
+    <p>
+      If you have a question regarding our privacy policy, wish to exercise any of your rights, or feel dissatisfaction
+      with our use of your data, please contact the Cloudnet team by sending an email to
+      <a href="mailto:actris-cloudnet-feedback@fmi.fi">actris-cloudnet-feedback@fmi.fi</a>. We will perform our duties
+      without undue delay within one month of your request. That period may be extended by two further months where
+      necessary, considering the complexity and number of the requests. For further information about your rights and
+      exercising them, please see here
+      <a href="https://tietosuoja.fi/en/know-your-rights">https://tietosuoja.fi/en/know-your-rights</a>.
+    </p>
     <h3>Changes to this policy</h3>
-    We will inform you of any material changes to this policy with a notice on our website.
-    <p></p>
+    <p>We will inform you of any material changes to this policy with a notice on our website.</p>
     <!-- REMEMBER TO CHANGE LAST UPDATED DATE WHEN MAKING MODIFICATIONS TO THIS PAGE. SEE ../lib/ActiveConsent.ts -->
-    This policy has been last updated in <b>{{ lastEdited }}</b
-    >.
+    <p>
+      This policy has been last updated in <b>{{ lastEdited }}</b
+      >.
+    </p>
   </main>
 </template>
 
