74c26c31d6260cdb5fc29152041d8eebe3eded6c
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-10-13 17:30:08 +0300

    Update backend/src/migration/1665655290096-AddHumanReadableNameToModel.ts
    Co-authored-by: Tuomas Siipola <tuomas.siipola@fmi.fi>

--- backend/src/migration/1665655290096-AddHumanReadableNameToModel.ts
+++ backend/src/migration/1665655290096-AddHumanReadableNameToModel.ts
@@ -4,7 +4,9 @@ export class AddHumanReadableNameToModel1665655290096 implements MigrationInterf
   name = "AddHumanReadableNameToModel1665655290096";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "model" ADD "humanReadableName" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "model" ADD "humanReadableName" character varying NULL`);
+    await queryRunner.query(`UPDATE "model" SET "humanReadableName" = "id"`);
+    await queryRunner.query(`ALTER TABLE "model" ALTER COLUMN "humanReadableName" SET NOT NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
