62a9955552219c5e5ccf6c239f86533272a4f5dd
Tuomas Siipola <tuomas.siipola@fmi.fi>
2022-10-14 13:34:30 +0300

    Use the same visualization component everywhere

--- frontend/src/components/Visualization.vue
+++ frontend/src/components/Visualization.vue
@@ -1,11 +1,36 @@
+<style scoped lang="sass">
+.caption
+  text-align: center
+  font-weight: 400
+
+.visualization
+  width: 100%
+  height: auto
+</style>
+
 <template>
+  <div v-if="caption">
+    <div class="caption">
+      {{ data.productVariable.humanReadableName }}
+    </div>
+    <img
+      :src="quicklookUrl + data.s3key"
+      :width="data.dimensions && data.dimensions.width"
+      :height="data.dimensions && data.dimensions.height"
+      alt=""
+      class="visualization"
+      :style="imageStyle"
+    />
+  </div>
+  <!-- TODO: remove legacy layout in the future. -->
   <img
+    v-else
     :src="quicklookUrl + data.s3key"
     :width="data.dimensions && data.dimensions.width"
     :height="data.dimensions && data.dimensions.height"
     alt=""
     class="visualization"
-    :style="style"
+    :style="imageStyle"
   />
 </template>
 
@@ -17,11 +42,18 @@ import { VisualizationItem } from "../../../backend/src/entity/VisualizationResp
 @Component
 export default class Visualization extends Vue {
   @Prop() data!: VisualizationItem;
-  @Prop() maxMarginLeft!: number;
-  @Prop() maxMarginRight!: number;
+  @Prop() maxMarginLeft?: number;
+  @Prop() maxMarginRight?: number;
+  @Prop({ default: false }) caption!: boolean;
 
-  get style() {
-    if (!this.data.dimensions) return {};
+  get imageStyle() {
+    if (
+      !this.data.dimensions ||
+      typeof this.maxMarginLeft === "undefined" ||
+      typeof this.maxMarginRight === "undefined"
+    ) {
+      return {};
+    }
     const left = this.maxMarginLeft - this.data.dimensions.marginLeft;
     const right = this.maxMarginRight - this.data.dimensions.marginRight;
     const width = this.data.dimensions.width + left + right;
