03aaeaf29eaa134508087d02c08b48a7908f6af3
Tuomas Siipola <tuomas.siipola@fmi.fi>
2022-12-14 17:08:33 +0200

    Remove obsolete files

--- backend/scripts/populate-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-INDIR=${1:-"data/"}
-N_PROCS=${2:-1}
-FILES=$(find $INDIR -name "*.nc" | sort)
-N_FILES=$(printf "%s\n" "${FILES[@]}" | wc -l)
-
-echo "PUTting $N_FILES files..."
-printf "%s\n" "${FILES[@]}" \
-    | xargs -n1 -P$N_PROCS scripts/put-nc-file.sh
--- backend/scripts/put-nc-file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dump=$(ncdump -xh $1)
-uuid=$(echo "$dump"|grep file_uuid|cut -f4 -d'"')
-echo -n "$(basename $1) "
-curl -X PUT http://localhost:3000/files/$uuid \
-    -H "Content-Type: application/xml" \
-    --data "$dump"
-echo
