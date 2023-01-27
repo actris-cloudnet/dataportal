9e4066062196ff7b909fcf5ee72fa1c1066e7860
Simo Tukiainen <simo.tukiainen@fmi.fi>
2023-01-27 15:47:30 +0200

    Use `includes()`

--- frontend/src/components/landing/DataOrigin.vue
+++ frontend/src/components/landing/DataOrigin.vue
@@ -56,7 +56,7 @@
             <br />
           </span>
           <span v-if="response.cloudnetpyVersion">
-            <template v-if="response.cloudnetpyVersion.indexOf('ustom') !== -1">
+            <template v-if="response.cloudnetpyVersion.toLowerCase().includes('custom')">
               {{ response.cloudnetpyVersion }}
             </template>
             <a v-else :href="`https://github.com/actris-cloudnet/cloudnetpy/tree/v${response.cloudnetpyVersion}`">
