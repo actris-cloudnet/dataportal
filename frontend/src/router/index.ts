abc83f43c9d5361c17ce423ccda360a15f12eb52
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-02-09 11:28:46 +0200

    Clarify comment

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -33,7 +33,8 @@ const routes = [
     redirect: "/search/data",
   },
   {
-    // Allow two slashes in URL. Investigate if this is really needed.
+    // Allow two slashes in URL. It's possible that some early PIDs resolve to a
+    // landing pages with two slashes.
     path: "/(/)?file/:uuid",
     meta: {
       title: "Cloudnet Data Object",
