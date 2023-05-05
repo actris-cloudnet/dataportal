88913e8b7cae59f99d9c6a29f00a69233236895a
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-05-05 17:04:57 +0300

    Show status indicators on sites page

--- backend/src/routes/site.ts
+++ backend/src/routes/site.ts
@@ -3,16 +3,19 @@ import { Connection, Repository } from "typeorm";
 import { hideTestDataFromNormalUsers, toArray } from "../lib";
 import { Site } from "../entity/Site";
 import { SiteLocation } from "../entity/SiteLocation";
+import { RegularFile } from "../entity/File";
 
 export class SiteRoutes {
   constructor(conn: Connection) {
     this.conn = conn;
     this.siteRepo = conn.getRepository<Site>("site");
+    this.regularFileRepo = conn.getRepository<RegularFile>("regular_file");
     this.siteLocationRepo = conn.getRepository<SiteLocation>("site_location");
   }
 
   readonly conn: Connection;
   readonly siteRepo: Repository<Site>;
+  readonly regularFileRepo: Repository<RegularFile>;
   readonly siteLocationRepo: Repository<SiteLocation>;
 
   site: RequestHandler = async (req: Request, res: Response, next) => {
@@ -31,21 +34,52 @@ export class SiteRoutes {
     const qb = this.siteRepo.createQueryBuilder("site");
     if (query.showCitations) qb.leftJoinAndSelect("site.citations", "citations");
     else qb.select();
+    const statuses = await this.fetchStatuses();
 
     hideTestDataFromNormalUsers(qb, req)
       .addOrderBy("site.id", "ASC")
       .getMany()
-      .then((result) => {
+      .then((sites) => {
         const typeQuery = toArray(query.type);
-        if (typeQuery) result = sitesContainAtLeastOneType(result, typeQuery);
-        res.send(result);
+        if (typeQuery) sites = this.sitesContainAtLeastOneType(sites, typeQuery);
+        res.send(
+          sites.map((site) => ({
+            ...site,
+            status: statuses[site.id] || "inactive",
+          }))
+        );
       })
       .catch((err) => next({ status: 500, errors: err }));
-
-    function sitesContainAtLeastOneType(result: Site[], typeQuery: Array<string>) {
-      return result.filter((site) => site.type.filter((type) => typeQuery.includes(type)).length > 0);
-    }
   };
+
+  private sitesContainAtLeastOneType(sites: Site[], typeQuery: Array<string>) {
+    return sites.filter((site) => site.type.filter((type) => typeQuery.includes(type)).length > 0);
+  }
+
+  private async fetchStatuses(): Promise<Record<string, string>> {
+    const rows = await this.regularFileRepo
+      .createQueryBuilder("file")
+      .select("file.siteId")
+      .addSelect("array_agg(distinct file.productId)", "latestProducts")
+      .where("file.measurementDate > CURRENT_DATE - 7")
+      .groupBy("file.siteId")
+      .getRawMany();
+
+    return rows.reduce((obj, item) => {
+      obj[item.siteId] = this.getStatus(item.latestProducts);
+      return obj;
+    }, {});
+  }
+
+  private getStatus(products: string[]) {
+    if (products.includes("classification")) {
+      return "cloudnet";
+    }
+    if (products.length > 0) {
+      return "active";
+    }
+    return "inactive";
+  }
 
   location: RequestHandler = async (req, res, next) => {
     try {
