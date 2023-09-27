ef9725a070b3d4b037fa618bb5ab4d25ab814b7c
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-09-27 11:51:36 +0300

    Simplify footer styles

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,65 +1,58 @@
-<style scoped lang="sass">
-@import "@/sass/variables.sass"
-@import "@/sass/new-variables.scss"
+<style scoped lang="scss">
+@import "@/sass/variables.sass";
+@import "@/sass/new-variables.scss";
 
-footer
-  background: #fdfdfd
-  box-shadow: 0 1px 3px rgba(0,0,0,0.3)
-  z-index: 5
-  bottom: 0
+footer {
+  background: #fdfdfd;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+}
 
-.footeritem-container
-  max-width: $page-width
-  height: $footer-height
-  margin: 0 auto
-  display: grid
-  grid-template-columns: repeat(3, 1fr)
-  padding: 0 1rem
-  box-sizing: content-box
+.container {
+  max-width: $page-width;
+  margin: 0 auto;
+  padding: 0 1rem;
+  box-sizing: content-box;
+  height: 7rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
 
-@media screen and (max-width: $narrow-screen)
-  .footeritem-container
-    display: flex
-    flex-direction: column
-    height: auto
-    padding: 1rem
-    gap: 1rem
+@media screen and (max-width: $narrow-screen) {
+  .container {
+    flex-direction: column;
+    height: auto;
+    padding: 1rem;
+    gap: 1rem;
+  }
+}
 
-.footeritem
-  color: darkgrey
-  line-height: 1.1
-  font-size: 95%
-  text-align: center
-  padding: 0
-  margin: 0
+.item {
+  color: darkgrey;
+  font-size: 95%;
+  text-align: center;
+}
 
-.footeritem.no-width
-  width: auto
+.small a,
+.hidden-links a {
+  color: inherit;
+}
 
-.footeritem a:focus
-  outline: thin dotted
-
-.small a, .hidden-links a
-  color: darkgrey
-
-.small
-  font-size: 0.6em
-
-.align-right
-  text-align: right
+.small {
+  font-size: 60%;
+}
 </style>
 
 <template>
-  <footer class="global">
-    <div class="footeritem-container">
-      <div class="vertical-center footeritem">
-        Contact:
-        <a href="mailto:actris-cloudnet@fmi.fi">actris-cloudnet@fmi.fi</a>
+  <footer>
+    <div class="container">
+      <div class="item">
+        Contact:<br />
+        <a href="mailto:actris-cloudnet@fmi.fi">actris-cloudnet@fmi.fi</a><br />
         <a href="https://github.com/actris-cloudnet/">GitHub organization</a>
       </div>
-      <div class="vertical-center footeritem centered hidden-links">
-        <span>&copy; <a href="https://en.ilmatieteenlaitos.fi/">Finnish Meteorological Institute</a></span
-        ><br />
+      <div class="item hidden-links">
+        <span> &copy; <a href="https://en.ilmatieteenlaitos.fi/">Finnish Meteorological Institute</a> </span><br />
         <span class="small">
           <a href="https://www.flickr.com/photos/theaucitron/5810163712/">Header photo</a>
           by <a href="https://www.flickr.com/photos/theaucitron/">theaucitron</a>,
@@ -67,8 +60,8 @@ footer
           Icons from <a href="https://icons8.com/">icons8.com</a>.
         </span>
       </div>
-      <div class="vertical-center footeritem">
-        <a href="https://docs.cloudnet.fmi.fi/api/data-portal.html">API documentation</a>
+      <div class="item">
+        <a href="https://docs.cloudnet.fmi.fi/api/data-portal.html">API documentation</a><br />
         <router-link to="/privacy">Privacy policy</router-link>
       </div>
     </div>
