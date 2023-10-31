da6e112637a00c1999d21cbc8e00801b0f7fdf9e
Simo Tukiainen <simo.tukiainen@fmi.fi>
2023-10-31 15:11:59 +0200

    Split software parsing to its own function

--- backend/src/lib/index.ts
+++ backend/src/lib/index.ts
@@ -85,16 +85,17 @@ export const augmentFile = (includeS3path: boolean) => (file: RegularFile | Mode
   s3key: undefined,
   s3path: includeS3path ? getS3pathForFile(file) : undefined,
   model: "model" in file ? file.model : undefined,
-  software: file.software
-    ? file.software.map((software) => ({
-        id: software.code,
-        version: software.version,
-        title: `${software.humanReadableName || software.code} ${software.version}`,
-        url: software.url,
-      }))
-    : undefined,
+  software: parseSoftware(file),
   timeliness: calcTimeliness(file),
 });
+
+const parseSoftware = (file: RegularFile | ModelFile) =>
+  file.software?.map((software) => ({
+    id: software.code,
+    version: software.version,
+    title: `${software.humanReadableName || software.code} ${software.version}`,
+    url: software.url,
+  }));
 
 function calcTimeliness(file: RegularFile | ModelFile) {
   if (!file.stopTime) {
