325f8f82ebb3b8d79cebb06011741209db8d019a
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-12-11 17:42:09 +0200

    Use `vue-matomo`

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from "vue";
+import VueMatomo from "vue-matomo";
+
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
 app.use(router);
+
+if (import.meta.env.VITE_MATOMO_HOST && import.meta.env.VITE_MATOMO_SITE_ID) {
+  app.use(VueMatomo, {
+    host: import.meta.env.VITE_MATOMO_HOST,
+    siteId: import.meta.env.VITE_MATOMO_SITE_ID,
+    router,
+    disableCookies: true,
+  });
+}
 
 app.mount("#app");
