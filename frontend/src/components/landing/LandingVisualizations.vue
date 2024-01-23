420a15d0968c82123d69918f2eb6205a8b6f251a
Simo Tukiainen <simo.tukiainen@fmi.fi>
2024-01-23 11:13:18 +0200

    Add link to visualisation search

--- frontend/src/components/landing/LandingVisualizations.vue
+++ frontend/src/components/landing/LandingVisualizations.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="landing-visualizations-container pagewidth">
     <div class="summary-box">
-      <div class="visualizations" v-if="visualizations.length > 0">
-        <Visualization
-          v-for="visualization in visualizations"
-          :key="visualization.productVariable.id"
-          :data="visualization"
-          :maxMarginLeft="maxMarginLeft"
-          :maxMarginRight="maxMarginRight"
-          expandable
-          linkToVocabulary
-        />
-      </div>
+      <template v-if="visualizations.length > 0">
+        <div class="visualizations" v-if="visualizations.length > 0">
+          <Visualization
+            v-for="visualization in visualizations"
+            :key="visualization.productVariable.id"
+            :data="visualization"
+            :maxMarginLeft="maxMarginLeft"
+            :maxMarginRight="maxMarginRight"
+            expandable
+            linkToVocabulary
+          />
+        </div>
+        <div class="link-to-viz">
+          <router-link :to="linkToVisualizationSearch">View in visualisation search</router-link>
+        </div>
+      </template>
       <div v-else>No visualisations available.</div>
     </div>
   </div>
@@ -53,6 +58,20 @@ const maxMarginRight = computed(() => {
   }
   return max;
 });
+
+const linkToVisualizationSearch = computed(() => {
+  const response = props.response;
+  return {
+    name: "Search",
+    params: { mode: "visualizations" },
+    query: {
+      site: response.site.id,
+      product: response.product.id,
+      dateFrom: response.measurementDate,
+      dateTo: response.measurementDate,
+    },
+  };
+});
 </script>
 
 <style scoped lang="scss">
@@ -60,5 +79,8 @@ const maxMarginRight = computed(() => {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(min(700px, 100%), 1fr));
   gap: 1rem;
+}
+.link-to-viz {
+  margin-top: 0.5rem;
 }
 </style>
