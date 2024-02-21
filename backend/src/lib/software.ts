c70546449f9d53a5e85a2c60fce3ed4ab14f38ce
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-02-21 15:22:55 +0200

    Fix MWRpy spelling

--- backend/src/lib/software.ts
+++ backend/src/lib/software.ts
@@ -24,7 +24,7 @@ export class SoftwareService {
     },
     "halo-reader": { humanReadableName: "HALO reader", githubRepository: "actris-cloudnet/halo-reader" },
     "doppy": { humanReadableName: "Doppy", githubRepository: "actris-cloudnet/doppy" },
-    "mwrpy": { humanReadableName: "MWRPy", githubRepository: "actris-cloudnet/mwrpy" },
+    "mwrpy": { humanReadableName: "MWRpy", githubRepository: "actris-cloudnet/mwrpy" },
     "voodoonet": {
       humanReadableName: "VoodooNet",
       githubRepository: "actris-cloudnet/voodoonet",
