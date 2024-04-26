0bf5e1afd62857735caaa6605e7f602ef77be0c6
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-04-26 14:40:15 +0300

    Implement tests for file page

--- frontend/tests/FileView.test.ts
+++ frontend/tests/FileView.test.ts
@@ -1,9 +1,11 @@
 import { VueWrapper, mount } from "@vue/test-utils";
-import FileView from "../src/views/FileView.vue";
+import App from "../src/App.vue";
 import axios, { AxiosPromise, AxiosRequestConfig } from "axios";
 import { augmentAxiosResponse, nextTick } from "./lib";
 import { findByUuid, readResources } from "../../shared/lib";
 import { vi, describe, beforeAll, expect, it } from "vitest";
+import { routes } from "../src/router";
+import { createRouter, createWebHistory } from "vue-router";
 
 vi.mock("axios");
 
@@ -33,7 +35,6 @@ const visualizationResponse = {
 
 let axiosMockWithFileUuid: Function;
 let resources: any;
-let wrapper: VueWrapper;
 
 describe("FileView.vue", () => {
   beforeAll(async () => {
@@ -59,177 +60,140 @@ describe("FileView.vue", () => {
           const i = Array.isArray(uuid) ? uuid[nreq] : uuid;
           nreq += 1;
           return Promise.resolve(augmentAxiosResponse(findByUuid(resources["allfiles"], i)));
+        } else if (url.match("/reference")) {
+          return Promise.resolve(augmentAxiosResponse("I'm a reference"));
         }
         return Promise.reject(new Error(`Unmocked URL: ${url}`));
       };
     };
   });
-  // it("displays a note on volatile file", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("bde"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   return expect(wrapper.text()).toContain("This is a volatile file.");
-  // });
-  it("does not display a note on stable file", async () => {
-    vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("6cb"));
-    wrapper = mount(FileView, {
-      props: { uuid: "6cb" },
-      global: {
-        stubs: ["router-link", "router-view"],
-      },
+
+  describe("volatile file", () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: routes,
     });
-    await nextTick(1);
-    expect(wrapper.text()).not.toContain("This is a volatile file.");
+    let wrapper: VueWrapper;
+
+    beforeAll(async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("bde"));
+      wrapper = mount(App, { global: { plugins: [router] } });
+      router.push({ name: "File", params: { uuid: "bde" } });
+      await router.isReady();
+    });
+
+    it("displays a note on volatile file", async () => {
+      expect(wrapper.find(".tags").text()).toContain("Volatile");
+      expect(wrapper.text()).toContain("This data object is volatile and may be updated in the future.");
+    });
+
+    it("displays last modified date", async () => {
+      expect(wrapper.text()).toContain("2020-02-20 10:56:19");
+    });
   });
 
-  // it("displays a note on legacy file", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("3bb"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   return expect(wrapper.text()).toContain("This is legacy data.");
-  // });
-
-  // it("displays link to next and newest version in oldest version", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
-  //   wrapper = mountVue(File);
-  //   await nextTick(3);
-  //   expect(wrapper.findAll("#previousVersion").length).toEqual(0);
-  //   expect(wrapper.find("#nextVersion").html()).toContain("/file/6cb32746-faf0-4057-9076-ed2e698dcf36");
-  //   expect(wrapper.find("#newestVersion").html()).toContain("/file/8bb32746-faf0-4057-9076-ed2e698dcf36");
-  // });
-  //
-  // it("displays link to next, previous and newest version in second-to-newest version", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("6cb"));
-  //   wrapper = mountVue(File);
-  //   await nextTick(3);
-  //   expect(wrapper.find("#nextVersion").html()).toContain("/file/8bb32746-faf0-4057-9076-ed2e698dcf36");
-  //   expect(wrapper.find("#previousVersion").html()).toContain("/file/22b32746-faf0-4057-9076-ed2e698dcc34");
-  //   expect(wrapper.find("#newestVersion").html()).toContain("/file/8bb32746-faf0-4057-9076-ed2e698dcf36");
-  // });
-  //
-  // it("displays link to previous version in the newest version", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("8bb"));
-  //   wrapper = mountVue(File);
-  //   await nextTick(3);
-  //   expect(wrapper.findAll("#newestVersion").length).toEqual(0);
-  //   expect(wrapper.find("#previousVersion").html()).toContain("/file/6cb32746-faf0-4057-9076-ed2e698dcf36");
-  // });
-  //
-  // it("displays last modified date", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("380"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   expect(wrapper.text()).toContain("2020-02-20 10:56:19 UTC");
-  // });
-  //
-  // it("displays links to source files", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid(["2bb", "1bb"]));
-  //   wrapper = mountVue(File);
-  //   await nextTick(3);
-  //   expect(wrapper.findAll("#history").length).toEqual(1);
-  //   expect(wrapper.find("#history").html()).toContain("/file/1bb32746-faf0-4057-9076-ed2e698dcf36");
-  // });
-
-  it("does not display source file info where not applicable", async () => {
-    vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("d21"));
-    wrapper = mount(FileView, {
-      props: { uuid: "d21" },
-      global: {
-        stubs: ["router-link", "router-view"],
-      },
+  describe("stable file", () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: routes,
     });
-    await nextTick(3);
-    expect(wrapper.findAll(".sourceFileList").length).toEqual(0);
+    let wrapper: VueWrapper;
+
+    beforeAll(async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid(["2bb", "1bb"]));
+      wrapper = mount(App, { global: { plugins: [router] } });
+      router.push({ name: "File", params: { uuid: "6cb" } });
+      await router.isReady();
+    });
+
+    it("does not display a note on stable file", () => {
+      expect(wrapper.text()).not.toContain("volatile");
+    });
+
+    it("displays PID", () => {
+      expect(wrapper.text()).toContain("http://pidservice.example/2bb");
+    });
+
+    it("displays links to source files", async () => {
+      const html = wrapper.find(".data-sources").html();
+      expect(html).toContain("Categorize");
+      expect(html).toContain("/file/1bb32746-faf0-4057-9076-ed2e698dcf36");
+    });
   });
 
-  // it("shows how to cite box by clicking a button", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
-  //   wrapper = mountVue(File, {
-  //     stubs: { "router-link": true, "how-to-cite": HowToCite },
-  //   });
-  //   await nextTick(2);
-  //   await wrapper.find("#showCiting").trigger("click");
-  //   await nextTick(1);
-  //   expect(wrapper.findAll("#howtocite").length).toEqual(1);
-  //   expect(wrapper.text()).toContain("This is an example of how to cite Cloudnet datasets.");
-  //   expect(wrapper.text()).toContain("Bucharest test citation.");
-  // });
-  //
-  // it("hides how to cite box by clicking a button", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
-  //   wrapper = mountVue(File, {
-  //     stubs: { "router-link": true, "how-to-cite": HowToCite },
-  //   });
-  //   await nextTick(2);
-  //   await wrapper.find("#showCiting").trigger("click");
-  //   expect(wrapper.findAll("#howtocite").length).toEqual(1);
-  //   await wrapper.find("#hideCiting").trigger("click");
-  //   expect(wrapper.findAll("#howtocite").length).toEqual(0);
-  // });
-  //
-  // it("shows licenseby clicking a button", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
-  //   wrapper = mountVue(File, {
-  //     stubs: { "router-link": true, license: License },
-  //   });
-  //   await nextTick(2);
-  //   await wrapper.find("#showLicense").trigger("click");
-  //   expect(wrapper.findAll("#license").length).toEqual(1);
-  //   expect(wrapper.text()).toMatch(
-  //     /.*Cloudnet data is licensed under a[\s\n]*Creative Commons Attribution 4\.0 international licence\..*/
-  //   );
-  // });
-  //
-  // it("hides license box by clicking a button", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
-  //   wrapper = mountVue(File, {
-  //     stubs: { "router-link": true, license: License },
-  //   });
-  //   await nextTick(2);
-  //   await wrapper.find("#showLicense").trigger("click");
-  //   expect(wrapper.findAll("#license").length).toEqual(1);
-  //   await wrapper.find("#hideLicense").trigger("click");
-  //   expect(wrapper.findAll("#license").length).toEqual(0);
-  // });
-
-  // it("displays a note on quality controlled file", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("acf"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   return expect(wrapper.text()).toContain("Quality Controlled (QC)");
-  // });
-
-  // it("displays a note on nrt file", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   return expect(wrapper.text()).toContain("Near Real Time (NRT)");
-  // });
-  //
-  // it("displays Pass on passing quality check", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("62b"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   return expect(wrapper.text()).toContain("Pass");
-  // });
-  //
-  // it("displays Some problems on passing quality check", async () => {
-  //   mocked(axios.get).mockImplementation(axiosMockWithFileUuid("acf"));
-  //   wrapper = mountVue(File);
-  //   await Vue.nextTick();
-  //   return expect(wrapper.text()).toContain("Some issues");
-  // });
-
-  it("displays n/a on no quality check", async () => {
-    vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("72b"));
-    wrapper = mount(FileView, {
-      props: { uuid: "72b" },
-      global: {
-        stubs: ["router-link", "router-view"],
-      },
+  describe("legacy file", () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: routes,
     });
-    await nextTick(1);
-    expect(wrapper.text()).not.toContain("Some issues");
-    expect(wrapper.text()).not.toContain("Pass");
+    let wrapper: VueWrapper;
+
+    beforeAll(async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("3bb"));
+      wrapper = mount(App, { global: { plugins: [router] } });
+      router.push({ name: "File", params: { uuid: "3bb" } });
+      await router.isReady();
+    });
+
+    it("displays a note on legacy file", () => {
+      expect(wrapper.find(".tags").text()).toContain("Legacy");
+      expect(wrapper.text()).toContain("This data object was produced using nonstandard processing.");
+    });
+
+    it("does not display source files", async () => {
+      expect(wrapper.findAll(".source-files").length).toEqual(0);
+    });
+  });
+
+  describe("versions", () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: routes,
+    });
+    let wrapper: VueWrapper;
+
+    beforeAll(async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("3bb"));
+    });
+
+    it("displays link to next and newest version in oldest version", async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("22b"));
+      wrapper = mount(App, { global: { plugins: [router] } });
+      router.push({ name: "File", params: { uuid: "22b" } });
+      await router.isReady();
+      await nextTick(3);
+
+      const banner = wrapper.find(".banner");
+      expect(banner.text()).toContain("There is a newer version of this data available.");
+      expect(banner.find("a").attributes()["href"]).toBe("/file/8bb32746-faf0-4057-9076-ed2e698dcf36");
+
+      expect(wrapper.findAll("#previousVersion").length).toEqual(0);
+      expect(wrapper.find("#nextVersion").attributes()["href"]).toBe("/file/6cb32746-faf0-4057-9076-ed2e698dcf36");
+    });
+
+    it("displays link to next, previous and newest version in second-to-newest version", async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("6cb"));
+      wrapper = mount(App, { global: { plugins: [router] } });
+      router.push({ name: "File", params: { uuid: "6cb" } });
+      await router.isReady();
+      await nextTick(3);
+
+      const banner = wrapper.find(".banner");
+      expect(banner.text()).toContain("There is a newer version of this data available.");
+      expect(banner.find("a").attributes()["href"]).toBe("/file/8bb32746-faf0-4057-9076-ed2e698dcf36");
+
+      expect(wrapper.find("#nextVersion").attributes()["href"]).toBe("/file/8bb32746-faf0-4057-9076-ed2e698dcf36");
+      expect(wrapper.find("#previousVersion").attributes()["href"]).toBe("/file/22b32746-faf0-4057-9076-ed2e698dcc34");
+    });
+
+    it("displays link to previous version in the newest version", async () => {
+      vi.mocked(axios.get).mockImplementation(axiosMockWithFileUuid("8bb"));
+      wrapper = mount(App, { global: { plugins: [router] } });
+      router.push({ name: "File", params: { uuid: "8bb" } });
+      await router.isReady();
+      await nextTick(3);
+      expect(wrapper.findAll(".banner").length).toEqual(0);
+      expect(wrapper.find("#previousVersion").attributes()["href"]).toBe("/file/6cb32746-faf0-4057-9076-ed2e698dcf36");
+    });
   });
 });
