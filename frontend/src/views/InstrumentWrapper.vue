842604c9a17447d2acc201d15a54c3a7f0d43447
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-05-27 11:23:01 +0300

    Use instrument icon

--- frontend/src/views/InstrumentWrapper.vue
+++ frontend/src/views/InstrumentWrapper.vue
@@ -3,7 +3,7 @@
     <LandingHeader :title="instrumentInfo.value.name" :subtitle="instrumentInfo.value.instrument.humanReadableName">
       <template #tabs>
         <router-link class="tab" :to="{ name: 'Instrument' }">
-          <img :src="overviewIcon" alt="" />
+          <img :src="getInstrumentIcon(instrumentInfo.value.instrument)" alt="" />
           Overview
         </router-link>
         <router-link class="tab" :to="{ name: 'Raw Files' }">
@@ -21,9 +21,8 @@ import { ref, onMounted, computed } from "vue";
 import axios from "axios";
 import { useRoute } from "vue-router";
 import LandingHeader from "@/components/LandingHeader.vue";
-import overviewIcon from "@/assets/icons/overview.png";
 import folderIcon from "@/assets/icons/folder.png";
-import { backendUrl } from "@/lib";
+import { backendUrl, getInstrumentIcon } from "@/lib";
 import type { InstrumentPidResult } from "@/components/instrument/InstrumentOverview.vue";
 import { useTitle } from "@/router";
 
