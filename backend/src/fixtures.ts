1e2471f9346dbcf1dce13068a2873eab4b86be43
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-08-20 11:25:59 +0300

    Add option to delete entities in fixtures.ts

--- backend/src/fixtures.ts
+++ backend/src/fixtures.ts
@@ -4,15 +4,90 @@ import { basename, join } from "path";
 import { argv } from "process";
 import { AppDataSource } from "./data-source";
 
-const truncate = argv[3] ? argv[3] == "TRUNCATE" : false;
+import readline = require("node:readline/promises");
+import process = require("node:process");
+
+const action = argv[3];
+if (action != "SYNC" && action != "APPEND" && action != "TRUNCATE") {
+  console.error("Action missing: SYNC, APPEND or TRUNCATE");
+  process.exit(1);
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const isJson = (filepath: string) => filepath.substring(filepath.length - 4) == "json";
 
 async function handleFile(dataSource: DataSource, filepath: string) {
   const repoName = basename(filepath).split("-")[1].split(".")[0];
   const repo = dataSource.getRepository(repoName);
-  if (truncate) await repo.query(`TRUNCATE TABLE ${repoName} RESTART IDENTITY CASCADE`);
-  return repo.save(JSON.parse((await fsp.readFile(filepath)).toString()));
+  console.log(`Processing ${repoName}...`);
+  if (action == "TRUNCATE") {
+    console.log("NOTE: Truncating all existing data");
+    await repo.query(`TRUNCATE TABLE ${repoName} RESTART IDENTITY CASCADE`);
+  }
+
+  const primaryColumns = repo.metadata.primaryColumns.map((column) => column.propertyName);
+  function getId(obj: any) {
+    const id: any = {};
+    for (const column of primaryColumns) {
+      id[column] = obj[column];
+    }
+    return id;
+  }
+
+  const fixtures = JSON.parse(await fsp.readFile(filepath, { encoding: "utf-8" }));
+  if (action === "SYNC") {
+    let insertObjs = [];
+    let updateObjs = [];
+    const existingIds = (await repo.find()).reduce((obj, row) => {
+      const id = getId(row);
+      obj[JSON.stringify(id)] = id;
+      return obj;
+    }, {});
+    for (const fixture of fixtures) {
+      const id = getId(fixture);
+      if (JSON.stringify(id) in existingIds) {
+        updateObjs.push([id, fixture]);
+      } else {
+        insertObjs.push([id, fixture]);
+      }
+      delete existingIds[JSON.stringify(id)];
+    }
+    const deleteIds = Object.values(existingIds);
+
+    if (insertObjs.length + updateObjs.length + deleteIds.length == 0) {
+      console.log("Nothing to do...");
+      return;
+    }
+    if (insertObjs.length > 0) {
+      console.log(
+        "Insert:",
+        insertObjs.map((a) => a[0]),
+      );
+    }
+    if (updateObjs.length > 0) {
+      console.log("Update:", updateObjs.length, updateObjs.length == 1 ? "entity" : "entities");
+    }
+    if (deleteIds.length > 0) {
+      console.log("Delete:", deleteIds);
+    }
+
+    const answer = await rl.question("Apply changes to database? [y/n] ");
+    if (answer.toLowerCase() != "y") {
+      console.log("I guess not then...");
+      return;
+    }
+
+    for (const id of deleteIds) {
+      await repo.delete(id);
+    }
+    await repo.save([...insertObjs.map((a) => a[0]), ...updateObjs.map((a) => a[0])]);
+  } else {
+    await repo.save(fixtures);
+  }
 }
 
 async function handleDir(dataSource: DataSource, dirpath: string) {
@@ -30,13 +105,9 @@ async function handleDir(dataSource: DataSource, dirpath: string) {
 async function importFixture(path: string) {
   const dataSource = await AppDataSource.initialize();
   const stat = await fsp.lstat(path);
-  if (stat.isDirectory()) return handleDir(dataSource, path);
-  else if (stat.isFile() && isJson(path)) return handleFile(dataSource, path);
+  if (stat.isDirectory()) await handleDir(dataSource, path);
+  else if (stat.isFile() && isJson(path)) await handleFile(dataSource, path);
   else throw "Unknown file type";
-}
-
-if (truncate) {
-  console.log("NOTE: Truncating all existing data");
 }
 
 importFixture(argv[2])
