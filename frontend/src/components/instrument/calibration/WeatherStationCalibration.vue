cf58519ae0a2230793ea006846b958e2fffa202f
Simo Tukiainen <simo.tukiainen@fmi.fi>
2024-09-30 11:45:15 +0300

    Add time offset calibration info to weather station

---
+++ frontend/src/components/instrument/calibration/WeatherStationCalibration.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <CalibrationTable
+      :data="timeOffset"
+      :measurementDates="measurementDates"
+      :timestamps="timestamps"
+      :config="{ title: 'Time offset', label: 'Offset (min)' }"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import CalibrationTable from "@/components/instrument/calibration/CalibrationTable.vue";
+
+const props = defineProps<{
+  measurementDates: string[];
+  timestamps: string[];
+  calibrationData: {
+    data: {
+      time_offset?: string[];
+    };
+  }[];
+}>();
+
+const timeOffset = computed(() => props.calibrationData.map((entry) => entry.data.time_offset ?? null));
+</script>
