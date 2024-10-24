e8449dd8699e57e1429bc1849e05c52bb4a75ac1
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-10-24 16:07:07 +0300

    End collection README with newline

--- backend/src/routes/download.ts
+++ backend/src/routes/download.ts
@@ -204,6 +204,6 @@ export class DownloadRoutes {
       "Cloudnet data is licensed under a Creative Commons Attribution 4.0 international licence.",
       "You should have received a copy of the license along with this work. If not, see <http://creativecommons.org/licenses/by/4.0/>.",
     ];
-    return lines.join("\n\n");
+    return lines.join("\n\n") + "\n";
   }
 }
