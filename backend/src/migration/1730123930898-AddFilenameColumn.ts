76d842c9f5793c89495fb3087466c85015f8a948
Tuomas Siipola <tuomas.siipola@fmi.fi>
2024-11-28 10:40:16 +0200

    sql

--- backend/src/migration/1730123930898-AddFilenameColumn.ts
+++ backend/src/migration/1730123930898-AddFilenameColumn.ts
@@ -6,10 +6,10 @@ export class AddFilenameColumn1730123930898 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "regular_file" ADD "filename" character varying`);
     await queryRunner.query(`UPDATE "regular_file" SET "filename" = regexp_replace(s3key, '.+/', '')`);
-    await queryRunner.query(`ALTER TABLE "regular_file" ALTER COLUMN "filename" SET NOT NULL;`);
+    await queryRunner.query(`ALTER TABLE "regular_file" ALTER COLUMN "filename" SET NOT NULL`);
     await queryRunner.query(`ALTER TABLE "model_file" ADD "filename" character varying`);
     await queryRunner.query(`UPDATE "model_file" SET "filename" = regexp_replace(s3key, '.+/', '')`);
-    await queryRunner.query(`ALTER TABLE "model_file" ALTER COLUMN "filename" SET NOT NULL;`);
+    await queryRunner.query(`ALTER TABLE "model_file" ALTER COLUMN "filename" SET NOT NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
