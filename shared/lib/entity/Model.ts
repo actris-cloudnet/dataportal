15c6cf4b8fbf83ac8e50b8214d87e39088d68657
Tuomas Siipola <tuomas.siipola@fmi.fi>
2025-02-24 16:25:26 +0200

    Fix shared definitions

--- shared/lib/entity/Model.ts
+++ shared/lib/entity/Model.ts
@@ -1,12 +1,8 @@
-import type { ModelUpload } from "./Upload";
-import type { ModelFile } from "./File";
 import type { ModelCitation } from "./Citation";
 
 export interface Model {
   id: string;
   humanReadableName: string;
   optimumOrder: number;
-  uploads: ModelUpload[];
-  files: ModelFile[];
   citations: ModelCitation[];
 }
--- shared/lib/entity/Product.ts
+++ shared/lib/entity/Product.ts
@@ -1,7 +1,5 @@
-import type { File } from "./File";
 import type { Instrument } from "./Instrument";
 import type { ProductVariable } from "./ProductVariable";
-import type { Upload } from "./Upload";
 
 export type ProductType = "instrument" | "model" | "evaluation" | "experimental" | "geophysical";
 
@@ -9,10 +7,8 @@ export interface Product {
   id: string;
   humanReadableName: string;
   experimental: boolean;
-  files: File[];
   type: ProductType[];
   variables: ProductVariable[];
-  uploads: Upload[];
   sourceInstruments: Instrument[];
   sourceProducts: Product[];
   derivedProducts: Product[];
