81b7aa925fa8c37391dd3e81958bf0f042785820
Niko Leskinen <niko.leskinen@fmi.fi>
2025-08-12 11:28:48 +0300

    Refactor product fetch

--- frontend/src/components/search/ProductFilter.vue
+++ frontend/src/components/search/ProductFilter.vue
@@ -1,53 +1,48 @@
 <template>
-<div class="filterbox">
-<CustomMultiselect
-label="Product"
-v-model="selectedProductIds"
-:options="productOptions"
-id="productSelect"
-:multiple="true"
-:getIcon="getProductIcon"
-/>
-<CheckBox
-id="showExpProductsCheckbox"
-class="checkbox"
-v-model="showExpProducts"
-label="Show experimental products"
-/>
-</div>
+  <div class="filterbox">
+    <CustomMultiselect
+      label="Product"
+      v-model="selectedProductIds"
+      :options="productOptions"
+      id="productSelect"
+      :multiple="true"
+      :getIcon="getProductIcon"
+    />
+    <CheckBox
+      id="showExpProductsCheckbox"
+      class="checkbox"
+      v-model="showExpProducts"
+      label="Show experimental products"
+    />
+  </div>
 </template>
 <script setup lang="ts">
-import { ref, computed, onMounted } from "vue";
-import axios from "axios";
-import type { Product } from "@shared/entity/Product";
+import { computed, onMounted } from "vue";
 import CustomMultiselect from "@/components/MultiSelect.vue";
 import CheckBox from "@/components/CheckBox.vue";
 import { useRouteQuery, queryBoolean, queryStringArray } from "@/lib/useRouteQuery";
-import { backendUrl, getProductIcon} from "@/lib";
-import { alphabeticalSort } from "@/lib/SearchUtils";
+import { getProductIcon } from "@/lib";
+import { useProducts } from "@/composables/useProduct";
 
 // products
-const allProducts = ref<Product[]>([]);
+const { allProducts, error: fetchError } = useProducts();
+
 const selectedProductIds = useRouteQuery({ name: "product", defaultValue: [], type: queryStringArray });
 const showExpProducts = useRouteQuery({ name: "experimental", defaultValue: false, type: queryBoolean });
 
 const productOptions = computed(() =>
-allProducts.value.filter((product) => showExpProducts.value || !product.experimental),
+  allProducts.value.filter((product) => showExpProducts.value || !product.experimental),
 );
 
-
 onMounted(async () => {
-const products = await axios.get<Product[]>(`${backendUrl}products/variables`);
-allProducts.value = products.data.sort(alphabeticalSort);
-
-if (
-!showExpProducts.value &&
-selectedProductIds.value.some((productId) => {
-const product = allProducts.value.find((product) => product.id === productId);
-return product && product.experimental;
-})
-) {
-showExpProducts.value = true;
-}
+  if (
+    !showExpProducts.value &&
+    selectedProductIds.value.some((productId) => {
+      const product = allProducts.value.find((product) => product.id === productId);
+      return product && product.experimental;
+    })
+  ) {
+    showExpProducts.value = true;
+  }
 });
 </script>
