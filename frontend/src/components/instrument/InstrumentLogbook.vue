f51a5d44455a8190a45175a08f45e877b3a59f85
Tuomas Siipola <tuomas.siipola@fmi.fi>
2025-10-23 11:58:18 +0300

    more mock logbook

--- frontend/src/components/instrument/InstrumentLogbook.vue
+++ frontend/src/components/instrument/InstrumentLogbook.vue
@@ -8,6 +8,72 @@ import BaseButton from "../BaseButton.vue";
 const selectedStatus = ref(null);
 const selectedOperation = ref(null);
 const selectedDate = ref(dateToString(new Date()));
+
+const schema = {
+  cl61d: {
+    maintenance: [
+      {
+        id: "cleanWindow",
+        title: "Clean window",
+        fields: [
+          {
+            id: "notes",
+            label: "Notes",
+            type: "textarea",
+            required: false,
+          },
+        ],
+      },
+      {
+        id: "terminationHood",
+        title: "Termination hood",
+        fields: [
+          {
+            id: "startTime",
+            label: "Start time",
+            type: "datetime",
+          },
+          {
+            id: "stopTime",
+            label: "Stop time",
+            type: "datetime",
+          },
+        ],
+      },
+      {
+        id: "replaceTransmitterModule",
+        title: "Replace transmitter module",
+        fields: [
+          {
+            id: "p1",
+            label: "Factory laser power value of the old transmitter (p1)",
+            type: "number",
+          },
+          {
+            id: "p2",
+            label: "Factory laser power value of the new transmitter (p2)",
+            type: "number",
+          },
+          {
+            id: "r",
+            label: "Adjustment ratio for the transmitter power (r = p1 / p2)",
+            type: "number",
+          },
+          {
+            id: "c1",
+            label: "Previous calibration scalar (c1)",
+            type: "number",
+          },
+          {
+            id: "c2",
+            label: "New adjusted calibration scalar (c2 = r × c1)",
+            type: "number",
+          },
+        ],
+      },
+    ],
+  },
+};
 </script>
 
 <template>
@@ -38,6 +104,28 @@ const selectedDate = ref(dateToString(new Date()));
         ]"
         style="margin-top: 0.5rem"
       />
+      <template v-if="selectedStatus == 'maintenance' && selectedOperation == 'replace-transmitter-module'">
+        <label>
+          Factory laser power value of the old transmitter (p1):<br />
+          <input type="text" />
+        </label>
+        <label>
+          Factory laser power value of the new transmitter (p2):<br />
+          <input type="text" />
+        </label>
+        <label>
+          Adjustment ratio for the transmitter power (r = p1 / p2):<br />
+          <input type="text" />
+        </label>
+        <label>
+          Previous calibration scalar (c1):<br />
+          <input type="text" />
+        </label>
+        <label>
+          New adjusted calibration scalar (c2 = r &times; c1):<br />
+          <input type="text" />
+        </label>
+      </template>
       <label
         v-if="selectedStatus == 'maintenance' ? selectedOperation != null : selectedStatus != null"
         style="display: block; margin-top: 0.5rem"
@@ -61,17 +149,32 @@ main {
   max-width: 600px;
 }
 
+label {
+  display: block;
+  margin-top: 0.5rem;
+}
+
+input[type="text"],
 textarea {
-  width: 100%;
+  width: 200px;
   padding: 0.5rem;
   border: 1px solid #ddd;
   border-radius: 4px;
-  margin-bottom: 0.5rem;
-  height: 150px;
-  resize: vertical;
+
+  &:focus-visible {
+    border: 1px solid transparent;
+    box-shadow: 0 0 0 2px slateblue;
+  }
 
   &::placeholder {
     color: gray;
   }
+}
+
+textarea {
+  width: 100%;
+  margin-bottom: 0.5rem;
+  height: 150px;
+  resize: vertical;
 }
 </style>
