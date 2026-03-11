5ef355e643ddcacb8f966b61897a6ed357b249f5
Tuomas Siipola <tuomas.siipola@fmi.fi>
2026-03-11 13:20:59 +0200

    Rename column instead of dropping and adding it

--- backend/src/migration/1773156113613-RenameLogCreatedByAddUpdatedBy.ts
+++ backend/src/migration/1773156113613-RenameLogCreatedByAddUpdatedBy.ts
@@ -5,8 +5,7 @@ export class RenameLogCreatedByAddUpdatedBy1773156113613 implements MigrationInt
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "instrument_log" DROP CONSTRAINT "FK_c08cfc482d4dabbd41ef74dd4e1"`);
-    await queryRunner.query(`ALTER TABLE "instrument_log" DROP COLUMN "userAccountId"`);
-    await queryRunner.query(`ALTER TABLE "instrument_log" ADD "createdById" integer`);
+    await queryRunner.query(`ALTER TABLE "instrument_log" RENAME COLUMN "userAccountId" TO "createdById"`);
     await queryRunner.query(`ALTER TABLE "instrument_log" ADD "updatedById" integer`);
     await queryRunner.query(
       `ALTER TABLE "instrument_log" ADD CONSTRAINT "FK_128b86971faec8d477a45097b87" FOREIGN KEY ("createdById") REFERENCES "user_account"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
@@ -20,8 +19,7 @@ export class RenameLogCreatedByAddUpdatedBy1773156113613 implements MigrationInt
     await queryRunner.query(`ALTER TABLE "instrument_log" DROP CONSTRAINT "FK_e4d23c7018ffe8d320ccdbcd134"`);
     await queryRunner.query(`ALTER TABLE "instrument_log" DROP CONSTRAINT "FK_128b86971faec8d477a45097b87"`);
     await queryRunner.query(`ALTER TABLE "instrument_log" DROP COLUMN "updatedById"`);
-    await queryRunner.query(`ALTER TABLE "instrument_log" DROP COLUMN "createdById"`);
-    await queryRunner.query(`ALTER TABLE "instrument_log" ADD "userAccountId" integer`);
+    await queryRunner.query(`ALTER TABLE "instrument_log" RENAME COLUMN "createdById" TO "userAccountId"`);
     await queryRunner.query(
       `ALTER TABLE "instrument_log" ADD CONSTRAINT "FK_c08cfc482d4dabbd41ef74dd4e1" FOREIGN KEY ("userAccountId") REFERENCES "user_account"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
     );
