1ddf1db5040bf613e8f018ad98168616df311824
Tuomas Siipola <tuomas.siipola@fmi.fi>
2026-06-11 10:53:59 +0300

    Fix missing person relation

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -85,7 +85,7 @@ export class Authenticator {
     }
     const user = await this.userRepo.findOne({
       where: { username: req.body.username, passwordHash: Not(IsNull()) },
-      relations: { permissions: true, instrumentLogPermissions: { instrumentInfo: true } },
+      relations: { person: true, permissions: true, instrumentLogPermissions: { instrumentInfo: true } },
     });
     if (!user || !(await this.hasPermission(user, PermissionType.canLogin))) {
       return next({ status: 401, errors: "Invalid username" });
